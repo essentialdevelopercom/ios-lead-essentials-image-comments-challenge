1bf8378a9ba6d711fd080be8eb1e1c188e0d1d04
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-10-21 19:48:21 -0500

    Does not request data on creation

---
+++ EssentialFeed/EssentialFeedTests/Comments Feature/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+import XCTest
+
+class RemoteImageCommentsLoader {
+    init(client _: HTTPClient) {}
+}
+
+final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
+    func test_init_doesNotPerformAnyURLRequest() {
+        let (_, client) = makeSUT()
+
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(
+        url _: URL = anyURL(),
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: RemoteImageCommentsLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteImageCommentsLoader(client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
+}
