c27e5f02460da79609e183f2dda985b36179fae9
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-10-21 21:54:07 -0500

    Does not deliver result after SUT instance has been deallocated

--- EssentialFeed/EssentialFeedTests/Comments/API/LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Comments/API/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -91,6 +91,20 @@ final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
         })
     }
 
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteImageCommentsLoader? = RemoteImageCommentsLoader(client: client)
+
+        var capturedResults = [RemoteImageCommentsLoader.Result]()
+        sut?.load(from: url) { capturedResults.append($0) }
+
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(
