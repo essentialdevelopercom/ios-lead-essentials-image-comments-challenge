1f1de67dd4b8602855fbb6826e3a8f08a0a9059c
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-10-25 16:29:20 -0600

    Records image with comments content

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentsViewController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentsViewController.swift
@@ -2,11 +2,40 @@
 //  Copyright © 2020 Essential Developer. All rights reserved.
 //
 
-import UIKit
 import EssentialFeed
+import UIKit
+
+class ImageCommentCell: UITableViewCell {
+    @IBOutlet var usernameLabel: UILabel?
+    @IBOutlet var createdAtLabel: UILabel?
+    @IBOutlet var commentLabel: UILabel?
+}
 
 public final class ImageCommentsViewController: UITableViewController, ImageCommentsView {
+    var models = [PresentableImageComment]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+
     public func display(_ viewModel: ImageCommentsViewModel) {
-        
+        models = viewModel.comments
+    }
+
+    override public func numberOfSections(in _: UITableView) -> Int {
+        return 1
+    }
+
+    override public func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return models.count
+    }
+
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ImageCommentCell") as! ImageCommentCell
+        let model = models[indexPath.row]
+        cell.usernameLabel?.text = model.username
+        cell.createdAtLabel?.text = model.createdAt
+        cell.commentLabel?.text = model.message
+        return cell
     }
 }
--- EssentialFeed/EssentialFeediOSTests/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsSnapshotTests.swift
@@ -16,6 +16,15 @@ final class ImageCommentsSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_EMPTY_COMMENTS_dark")
     }
 
+    func test_imageWithComments() {
+        let sut = makeSUT()
+
+        sut.display(imageComments())
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_WITH_COMMENTS_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_WITH_COMMENTS_dark")
+    }
+
     // MARK: - Helpers
 
     private func makeSUT() -> ImageCommentsViewController {
@@ -30,5 +39,29 @@ final class ImageCommentsSnapshotTests: XCTestCase {
 
     private func noComments() -> ImageCommentsViewModel {
         ImageCommentsViewModel(comments: [])
+    }
+
+    private func imageComments() -> ImageCommentsViewModel {
+        let comment1 = PresentableImageComment(
+            username: "Alfredo Hernandez",
+            createdAt: "1 week ago",
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        )
+        let comment2 = PresentableImageComment(
+            username: "Caio Zullo",
+            createdAt: "2 weeks ago",
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        )
+        let comment3 = PresentableImageComment(
+            username: "Mike Apostolakis",
+            createdAt: "1 hour ago",
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit\n.\n.\n.\n.\n✅"
+        )
+        let comment4 = PresentableImageComment(
+            username: "Hernandez Alfredo",
+            createdAt: "1 week ago",
+            message: "☀️ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ☀️"
+        )
+        return ImageCommentsViewModel(comments: [comment1, comment2, comment3, comment4])
     }
 }
