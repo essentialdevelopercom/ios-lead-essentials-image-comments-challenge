366364dcce08010c690809341bf0b2826b6d2503
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2020-11-22 12:17:49 +0100

    Replace 'ImageCommentsViewControllerTests.LoaderSpy' references in the production types with the production 'ImageCommentsLoader' protocol abstraction

--- EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
@@ -7,17 +7,19 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class ImageCommentsViewController: UIViewController {
-    private var loader: ImageCommentsViewControllerTests.LoaderSpy?
+    private var loader: ImageCommentsLoader?
     
-    convenience init(loader: ImageCommentsViewControllerTests.LoaderSpy) {
+    convenience init(loader: ImageCommentsLoader) {
         self.init()
         self.loader = loader
     }
     
     override func viewDidLoad() {
-        loader?.load()
+        let url = URL(string: "http://any-url.com")!
+        _ = loader?.loadComments(from: url) { _ in }
     }
     
 }
@@ -43,12 +45,18 @@ final class ImageCommentsViewControllerTests: XCTestCase {
     
     // MARK: - Helpers
     
-    class LoaderSpy {
+    class LoaderSpy: ImageCommentsLoader {
         private(set) var loadCallsCount = 0
         
-        func load() {
-            loadCallsCount += 1
+        private struct TaskSpy: ImageCommentsLoaderTask {
+            func cancel() {}
         }
+        
+        func loadComments(from url: URL, completion: @escaping (ImageCommentsLoader.Result) -> Void) -> ImageCommentsLoaderTask {
+            loadCallsCount += 1
+            return TaskSpy()
+        }
+
     }
 
 }
