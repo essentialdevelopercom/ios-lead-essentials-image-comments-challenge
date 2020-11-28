28a26f87b2aace28234815d5530328b50e090d01
Maxim Soldatov <maxim.soldatov@me.com>
2020-11-28 12:34:45 +0300

    Guarantee that we can call 'load' function more than once

--- EssentialFeed/EssentialFeedTests/Feed Comments/LoadFeedCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Comments/LoadFeedCommentsFromRemoteUseCaseTests.swift
@@ -37,6 +37,16 @@ class LoadFeedCommentsFromRemoteUseCaseTests: XCTestCase {
 		XCTAssertEqual(client.requestedURLs, [url])
 	}
 	
+	func test_loadTwice_requestsDataFromURLTwice() {
+		let url = anyURL()
+		let (sut, client) = makeSUT(url: url)
+		
+		sut.load()
+		sut.load()
+		
+		XCTAssertEqual(client.requestedURLs, [url, url])
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(url: URL = anyURL(),file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedCommentsLoader, client: HTTPClientSpy) {
