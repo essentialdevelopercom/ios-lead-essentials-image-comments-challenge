f3d1db4461a8c35c28a787dba6e0baeff8746214
Maxim Soldatov <maxim.soldatov@me.com>
2020-11-28 12:19:53 +0300

    'RemoteFeedCommentsLoader' does not request data on creation

---
+++ EssentialFeed/EssentialFeedTests/Feed Comments/LoadFeedCommentsFromRemoteUseCaseTests.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Maxim Soldatov on 11/28/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+import XCTest
+import EssentialFeed
+
+final class RemoteFeedCommentsLoader {
+	
+	init(client: HTTPClient) {
+		
+	}
+}
+
+class LoadFeedCommentsFromRemoteUseCaseTests: XCTestCase {
+	
+	func test_init_doesNotRequestDataFromURL() {
+		let (_, client) = makeSUT()
+		
+		XCTAssertTrue(client.requestedURLs.isEmpty)
+	}
+	
+	// MARK: - Helpers
+	
+	private func makeSUT(url: URL = anyURL(),file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedCommentsLoader, client: HTTPClientSpy) {
+		let client = HTTPClientSpy()
+		let sut = RemoteFeedCommentsLoader(client: client)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		trackForMemoryLeaks(client, file: file, line: line)
+		return (sut, client)
+	}
+}
