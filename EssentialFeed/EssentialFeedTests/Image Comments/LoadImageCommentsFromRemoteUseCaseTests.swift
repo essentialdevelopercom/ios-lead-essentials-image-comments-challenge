c91e99ee90151ad7af3b16d4d46affe112d6e743
RakeshR <rakeshr1983@gmail.com>
2020-12-01 12:20:06 +0530

    Added unit test to make sure RemoteImageCommentsLoader requests data from URL when 'load' method called.

--- EssentialFeed/EssentialFeedTests/Image Comments/LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -9,15 +9,42 @@
 import XCTest
 import EssentialFeed
 
+struct ImageComment {
+	let id: UUID
+	let message: String
+	let createdAt: Date
+	let author: String
+}
+
 class RemoteImageCommentsLoader {
-	init(client _: HTTPClient) {}
+	let client: HTTPClient
+
+	init(client: HTTPClient) {
+		self.client = client
+	}
+
+	func load(from url: URL, completion: @escaping (Result<[ImageComment], Error>) -> Void) {
+		client.get(from: url) { _ in
+		}
+	}
 }
 
 class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
+	
 	func test_init_doesNotPerformAnyURLRequest() {
 		let client = HTTPClientSpy()
 		let _ = RemoteImageCommentsLoader(client: client)
 
 		XCTAssertTrue(client.requestedURLs.isEmpty)
+	}
+	
+	func test_load_requestsDataFromURL() {
+		let url = URL(string: "https://a-given-url.com")!
+		let client = HTTPClientSpy()
+		let sut = RemoteImageCommentsLoader(client: client)
+
+		sut.load(from: url) { _ in }
+
+		XCTAssertEqual(client.requestedURLs, [url])
 	}
 }
