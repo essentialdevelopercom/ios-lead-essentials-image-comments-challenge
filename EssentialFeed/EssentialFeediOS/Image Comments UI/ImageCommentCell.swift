bfe596d5bfdc735c49a8bc4be040ed1fcee20fd0
RakeshR <rakesh1983@gmail.com>
2020-12-01 18:58:37 +0530

    Moved 'ImageCommentCell' to its own file

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Rakesh Ramamurthy on 01/12/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+class ImageCommentCell: UITableViewCell {
+	@IBOutlet var usernameLabel: UILabel?
+	@IBOutlet var createdAtLabel: UILabel?
+	@IBOutlet var commentLabel: UILabel?
+}
