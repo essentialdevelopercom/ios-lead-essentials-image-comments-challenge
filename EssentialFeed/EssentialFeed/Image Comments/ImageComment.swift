da03d4dc2d70dfa48b02c5a81efe4cdff382cf00
Rakesh Ramamurthy <rramamur@visa.com>
2020-12-01 16:52:56 +0530

    Added 'PresentableImageComment' for better separation and formats date to display relative date time

--- EssentialFeed/EssentialFeed/Image Comments/ImageComment.swift
+++ EssentialFeed/EssentialFeed/Image Comments/ImageComment.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 public struct ImageComment: Hashable {
-	let id: UUID
-	let message: String
-	let createdAt: Date
-	let author: String
+	public let id: UUID
+	public let message: String
+	public let createdAt: Date
+	public let author: String
 	
 	public init(id: UUID, message: String, createdAt: Date, author: String) {
 		self.id = id
