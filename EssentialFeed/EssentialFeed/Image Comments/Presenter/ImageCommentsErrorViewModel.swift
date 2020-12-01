4ba0a7eeb5db35d408427647f2e446cf5e08a023
RakeshR <rakesh1983@gmail.com>
2020-12-01 17:07:16 +0530

    Moved ImageCommentsErrorViewModel, ImageCommentsLoadingViewModel, ImageCommentsPresenter, PresentableImageComment to their own files for production.

---
+++ EssentialFeed/EssentialFeed/Image Comments/Presenter/ImageCommentsErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCommentsErrorViewModel.swift
+//  EssentialFeed
+//
+//  Created by Rakesh Ramamurthy on 01/12/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public struct ImageCommentsErrorViewModel {
+	public let errorMessage: String?
+}
---
+++ EssentialFeed/EssentialFeed/Image Comments/Presenter/PresentableImageComment.swift
@@ -0,0 +1,25 @@
+//
+//  PresentableImageComment.swift
+//  EssentialFeed
+//
+//  Created by Rakesh Ramamurthy on 01/12/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public struct PresentableImageComment: Hashable {
+	public let username: String
+	public let createdAt: String
+	public let message: String
+
+	public init(username: String, createdAt: String, message: String) {
+		self.username = username
+		self.createdAt = createdAt
+		self.message = message
+	}
+}
+
+public struct ImageCommentsViewModel {
+	public let comments: [PresentableImageComment]
+}
