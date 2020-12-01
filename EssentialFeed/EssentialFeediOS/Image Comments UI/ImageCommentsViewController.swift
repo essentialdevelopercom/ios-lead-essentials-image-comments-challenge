b723de60b48d180fa1e08129efc1d155aaba6427
RakeshR <rakesh1983@gmail.com>
2020-12-01 18:45:02 +0530

    Adding snapshot tests for no image comments use case

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCommentsViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Rakesh Ramamurthy on 01/12/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class ImageCommentsViewController: UITableViewController, ImageCommentsView {
+	public func display(_ viewModel: ImageCommentsViewModel) {
+
+	}
+}
