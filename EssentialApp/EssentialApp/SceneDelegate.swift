a6b11b9b06c61132bb95b7f168ca8eb4ac7db2d7
RakeshR <rakeshr1983@gmail.com>
2020-12-02 11:38:51 +0530

    Image selection implementation and tests

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -56,8 +56,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		window?.rootViewController = UINavigationController(
 			rootViewController: FeedUIComposer.feedComposedWith(
 				feedLoader: makeRemoteFeedLoaderWithLocalFallback,
-				imageLoader: makeLocalImageLoaderWithRemoteFallback))
-		
+				imageLoader: makeLocalImageLoaderWithRemoteFallback,
+				didSelectImage: { _ in }
+			)
+		)
+
 		window?.makeKeyAndVisible()
 	}
 	
