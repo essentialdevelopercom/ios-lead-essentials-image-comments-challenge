47f6f343280993988abd3ac91c11229c3f673614
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2020-12-06 11:48:52 +0100

    'ImageCommentsPresenter' displays comments and stops loading on 'didFinishLoadingComments'

--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -7,6 +7,15 @@
 //
 
 import XCTest
+import EssentialFeed
+
+struct ImageCommentsViewModel {
+    let comments: [ImageComment]
+}
+
+protocol ImageCommentsView {
+    func display(_ viewModel: ImageCommentsViewModel)
+}
 
 struct ImageCommentsLoadingViewModel {
     let isLoading: Bool
@@ -29,10 +38,12 @@ protocol ImageCommentsErrorView {
 }
 
 final class ImageCommentsPresenter {
+    private let imageCommentsView: ImageCommentsView
     private let loadingView: ImageCommentsLoadingView
     private let errorView: ImageCommentsErrorView
     
-    init(loadingView: ImageCommentsLoadingView, errorView: ImageCommentsErrorView) {
+    init(imageCommentsView: ImageCommentsView, loadingView: ImageCommentsLoadingView, errorView: ImageCommentsErrorView) {
+        self.imageCommentsView = imageCommentsView
         self.loadingView = loadingView
         self.errorView = errorView
     }
@@ -40,6 +51,11 @@ final class ImageCommentsPresenter {
     func didStartLoadingComments() {
         errorView.display(.noError)
         loadingView.display(ImageCommentsLoadingViewModel(isLoading: true))
+    }
+    
+    func didFinishLoadingComments(with comments: [ImageComment]) {
+        imageCommentsView.display(ImageCommentsViewModel(comments: comments))
+        loadingView.display(ImageCommentsLoadingViewModel(isLoading: false))
     }
 }
 
@@ -59,24 +75,41 @@ class ImageCommentsPresenterTests: XCTestCase {
         XCTAssertEqual(view.messages, [.display(errorMessage: .none), .display(isLoading: true)])
     }
     
+    func test_didFinishLoadingComments_displaysCommentsAndStopLoading() {
+        let (sut, view) = makeSUT()
+        
+        let comments = [comment(), comment()]
+        sut.didFinishLoadingComments(with: comments)
+        
+        XCTAssertEqual(view.messages, [.display(comments: comments), .display(isLoading: false)])
+    }
+
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: ImageCommentsPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = ImageCommentsPresenter(loadingView: view, errorView: view)
+        let sut = ImageCommentsPresenter(imageCommentsView: view, loadingView: view, errorView: view)
         trackForMemoryLeaks(view, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
-
     
-    private class ViewSpy: ImageCommentsLoadingView, ImageCommentsErrorView {
+    func comment() -> ImageComment {
+        return ImageComment(id: UUID(), message: "any", createdAt: Date(), username: "any")
+    }
+
+    private class ViewSpy: ImageCommentsView, ImageCommentsLoadingView, ImageCommentsErrorView {
         enum Message: Equatable {
             case display(errorMessage: String?)
             case display(isLoading: Bool)
+            case display(comments: [ImageComment])
         }
         
         private(set) var messages = [Message]()
+        
+        func display(_ viewModel: ImageCommentsViewModel) {
+            messages.append(.display(comments: viewModel.comments))
+        }
         
         func display(_ viewModel: ImageCommentsLoadingViewModel) {
             messages.append(.display(isLoading: viewModel.isLoading))
