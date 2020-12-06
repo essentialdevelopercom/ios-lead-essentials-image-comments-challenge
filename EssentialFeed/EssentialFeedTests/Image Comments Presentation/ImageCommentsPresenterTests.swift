c1ad2f87c9fb5f3e8fe027c14f862760e9ae836e
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2020-12-06 11:13:12 +0100

    Extract system under test creation to a test factory

--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -18,11 +18,21 @@ final class ImageCommentsPresenter {
 class ImageCommentsPresenterTests: XCTestCase {
     
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
-        _ = ImageCommentsPresenter(view: view)
-        
+        let (_, view) = makeSUT()
+       
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: ImageCommentsPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = ImageCommentsPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
+
     
     private class ViewSpy {
         private(set) var messages = [Any]()
