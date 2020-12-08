dd322cd7943a0ff5973eec3f23797a64b0a19d67
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2020-12-08 13:22:31 +0100

    Record comments with content snapshot

--- EssentialFeed/EssentialFeediOSTests/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsSnapshotTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import EssentialFeed
 import EssentialFeediOS
 
 class ImageCommentsSnapshotTests: XCTestCase {
@@ -18,6 +19,16 @@ class ImageCommentsSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "EMPTY_COMMENTS_light")
         record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_COMMENTS_dark")
     }
+    
+    func test_commentsWithContent() {
+        let sut = makeSUT()
+        
+        sut.display(commentsWithContent())
+
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "COMMENTS_WITH_CONTENT_light")
+        record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "COMMENTS_WITH_CONTENT_dark")
+    }
+
     
     // MARK: - Helpers
 
@@ -33,5 +44,33 @@ class ImageCommentsSnapshotTests: XCTestCase {
     
     private func emptyComments() -> [ImageCommentCellController] {
         return []
+    }
+    
+    private func commentsWithContent() -> [ImageComment] {
+        return [
+            ImageComment(
+                id: UUID(),
+                message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tortor magna, porta at sem vitae, bibendum tincidunt nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; ",
+                createdAt: Date.distantPast,
+                username: "username0"
+            ),
+            ImageComment(
+                id: UUID(),
+                message: "Nulla blandit condimentum tempor.",
+                createdAt: Date.distantPast,
+                username: "username1"
+            )
+        ]
+    }
+}
+
+private extension ImageCommentsViewController {
+    func display(_ comments: [ImageComment]) {
+        let cells: [ImageCommentCellController] = comments.map { comment in
+            let cellController = ImageCommentCellController(model: comment)
+            return cellController
+        }
+        
+        display(cells)
     }
 }
