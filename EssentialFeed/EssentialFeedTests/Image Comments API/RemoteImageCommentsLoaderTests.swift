7a30f9d87ccdd3e2b717bc0eae38e45b9425c10d
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-12-13 13:08:44 +0100

    `load` does request data from url

--- EssentialFeed/EssentialFeedTests/Image Comments API/RemoteImageCommentsLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/RemoteImageCommentsLoaderTests.swift
@@ -10,8 +10,16 @@ import XCTest
 import EssentialFeed
 
 class RemoteImageCommentsLoader {
-	init(client: HTTPClient) {
+	private let client: HTTPClient
+	private let url: URL
 
+	init(client: HTTPClient, url: URL) {
+		self.client = client
+		self.url = url
+	}
+
+	func load() {
+		client.get(from: url) { _ in }
 	}
 }
 
@@ -19,8 +27,18 @@ class RemoteImageCommentsLoaderTests: XCTestCase {
 
 	func test_init_doesNotRequestFromURL() {
 		let client = HTTPClientSpy()
-		_ = RemoteImageCommentsLoader(client: client)
+		_ = RemoteImageCommentsLoader(client: client, url: anyURL())
 
 		XCTAssertTrue(client.requestedURLs.isEmpty)
+	}
+
+	func test_load_requestsFromURL() {
+		let commentsURL = URL(string: "http://comments-url.com")!
+		let client = HTTPClientSpy()
+		let sut = RemoteImageCommentsLoader(client: client, url: commentsURL)
+
+		sut.load()
+
+		XCTAssertEqual(client.requestedURLs, [commentsURL])
 	}
 }
