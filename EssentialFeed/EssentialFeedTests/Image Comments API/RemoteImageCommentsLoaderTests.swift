a827e9e00fb284e2c66fa4bdef5da69ed78dad2d
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-12-13 13:00:04 +0100

    `RemoteImageCommentsLoader` does not request URL from client upon initialization

---
+++ EssentialFeed/EssentialFeedTests/Image Comments API/RemoteImageCommentsLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteImageCommentsLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Cronay on 13.12.20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class RemoteImageCommentsLoader {
+	init(client: HTTPClient) {
+
+	}
+}
+
+class RemoteImageCommentsLoaderTests: XCTestCase {
+
+	func test_init_doesNotRequestFromClientUponInitilization() {
+		let client = HTTPClientSpy()
+		_ = RemoteImageCommentsLoader(client: client)
+
+		XCTAssertTrue(client.requestedURLs.isEmpty)
+	}
+}
