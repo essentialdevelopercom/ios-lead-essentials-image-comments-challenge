cc2da90e8223563d224fbc2790ced786ef642b08
Maxim Soldatov <maxim.soldatov@me.com>
2020-12-15 08:36:51 +0300

    Remove 'class' constraint for 'FeedImageCommentsViewControllerDelegate' protocol

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCommentsViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCommentsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import EssentialFeed
 
-public protocol FeedImageCommentsViewControllerDelegate: class {
+public protocol FeedImageCommentsViewControllerDelegate {
 	func didRequestCommentsRefresh()
 }
 
