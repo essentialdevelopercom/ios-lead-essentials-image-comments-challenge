0fb49f321061ce1054e80511bc7665bb7d803e8a
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-12-20 14:11:19 +0100

    Title of `ImageCommentsPresenter` is localized

---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCommentsPresenter.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 20.12.20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public class ImageCommentsPresenter {
+
+	public static var title: String {
+		return NSLocalizedString("IMAGE_COMMENTS_VIEW_TITLE",
+			 tableName: "ImageComments",
+			 bundle: Bundle(for: ImageCommentsPresenter.self),
+			 comment: "Title for Image Comments view")
+	}
+
+}
