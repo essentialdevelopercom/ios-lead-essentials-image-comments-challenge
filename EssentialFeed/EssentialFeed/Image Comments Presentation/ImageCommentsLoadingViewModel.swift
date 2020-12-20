00856ed6db3d73b0173536d13b228574aa8ac730
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-12-20 14:58:28 +0100

    Move `ImageCommentsLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsLoadingViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentsLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Cronay on 20.12.20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public struct ImageCommentsLoadingViewModel {
+	public let isLoading: Bool
+}
+
