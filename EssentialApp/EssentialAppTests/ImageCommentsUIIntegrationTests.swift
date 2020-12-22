9bccdc4aef0f0723529a612df4132d2ca94cea47
Yannic Borgfeld <inf@yannic-borgfeld.de>
2020-12-22 22:41:37 +0100

    Image Comments UI requests comment when view is loaded as well as when the user initiates a reload

--- EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
@@ -13,26 +13,57 @@ final class ImageCommentUIComposer {
 
 	private init() {}
 
-	static func makeUI() -> ImageCommentsViewController {
+	static func makeUI(loader: ImageCommentsLoader) -> ImageCommentsViewController {
 		let controller = ImageCommentsViewController()
 		controller.title = ImageCommentsPresenter.title
+		controller.loader = loader
 		return controller
 	}
 }
 
-class ImageCommentsViewController: UIViewController {
+class ImageCommentsViewController: UITableViewController {
 
+	var loader: ImageCommentsLoader?
+
+	override func viewDidLoad() {
+		refreshControl = UIRefreshControl()
+		refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+
+		_ = loader?.load { _ in }
+	}
+
+	@objc private func refresh() {
+		_ = loader?.load { _ in }
+	}
 }
 
 class ImageCommentsUIIntegrationTests: XCTestCase {
 
 	func test_imageCommentsView_hasTitle() {
-		let sut = ImageCommentUIComposer.makeUI()
+		let loader = LoaderSpy()
+		let sut = ImageCommentUIComposer.makeUI(loader: loader)
 
 		sut.loadViewIfNeeded()
 
 		XCTAssertEqual(sut.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
 	}
+
+	func test_loadImageCommentsAction_requestsCommentsFromLoader() {
+		let loader = LoaderSpy()
+		let sut = ImageCommentUIComposer.makeUI(loader: loader)
+		XCTAssertEqual(loader.loadCount, 0)
+
+		sut.loadViewIfNeeded()
+		XCTAssertEqual(loader.loadCount, 1)
+
+		sut.simulateUserInitiatedReload()
+		XCTAssertEqual(loader.loadCount, 2)
+
+		sut.simulateUserInitiatedReload()
+		XCTAssertEqual(loader.loadCount, 3)
+	}
+
+	// MARK: - Helpers
 
 	func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
 		let table = "ImageComments"
@@ -44,4 +75,22 @@ class ImageCommentsUIIntegrationTests: XCTestCase {
 		return value
 	}
 
+	private class LoaderSpy: ImageCommentsLoader {
+		var loadCount = 0
+
+		private class Task: ImageCommentsLoaderTask {
+			func cancel() {}
+		}
+
+		func load(completion: @escaping (ImageCommentsLoader.Result) -> Void) -> ImageCommentsLoaderTask {
+			loadCount += 1
+			return Task()
+		}
+	}
+}
+
+extension ImageCommentsViewController {
+	func simulateUserInitiatedReload() {
+		refreshControl?.simulatePullToRefresh()
+	}
 }
