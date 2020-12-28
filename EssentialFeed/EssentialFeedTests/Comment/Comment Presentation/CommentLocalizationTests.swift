6b6e139314c37d5f913fbae9bf077de33c608ecd
knn <nguyenkhoi2190@gmail.com>
2020-12-28 17:21:33 +0800

    Add localisation strings and tests for Comment feature

--- EssentialFeed/EssentialFeedTests/Comment/Comment Presentation/CommentLocalizationTests.swift
+++ EssentialFeed/EssentialFeedTests/Comment/Comment Presentation/CommentLocalizationTests.swift
@@ -7,7 +7,25 @@
 //
 
 import XCTest
+import EssentialFeed
 
-class CommentLocalizationTests: XCTestCase {
-
+class CommentLocalizationTests: XCTestCase, LocalizationTest {
+	func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+		let table = "Comment"
+		let presentationBundle = Bundle(for: FeedPresenter.self)
+		let localizationBundles = allLocalizationBundles(in: presentationBundle)
+		let localizedStringKeys = allLocalizedStringKeys(in: localizationBundles, table: table)
+		
+		localizationBundles.forEach { (bundle, localization) in
+			localizedStringKeys.forEach { key in
+				let localizedString = bundle.localizedString(forKey: key, value: nil, table: table)
+				
+				if localizedString == key {
+					let language = Locale.current.localizedString(forLanguageCode: localization) ?? ""
+					
+					XCTFail("Missing \(language) (\(localization)) localized string for key: '\(key)' in table: '\(table)'")
+				}
+			}
+		}
+	}
 }
