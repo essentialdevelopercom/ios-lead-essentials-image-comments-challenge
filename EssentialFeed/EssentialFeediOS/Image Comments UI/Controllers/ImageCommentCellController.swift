4e4ccabc192351cd7b7f733e6b33dd972801da17
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2020-12-29 15:00:36 +0100

    Remove ImageCommentCellController cell property. The ImageCommentCellController doesn't do anything with the cell after creating it, so there's no need to hold it as a property. The table view will hold a strong reference to the cell.

--- EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -11,17 +11,16 @@ import EssentialFeed
 
 public final class ImageCommentCellController {
     private let model: ImageCommentViewModel
-    private var cell: ImageCommentCell?
     
     public init(model: ImageCommentViewModel) {
         self.model = model
     }
     
     func view(in tableView: UITableView) -> UITableViewCell {
-        cell = tableView.dequeueReusableCell()
-        cell?.author.text = model.username
-        cell?.date.text = model.date
-        cell?.message.text = model.message
-        return cell!
+		let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.author.text = model.username
+        cell.date.text = model.date
+        cell.message.text = model.message
+        return cell
     }
 }
