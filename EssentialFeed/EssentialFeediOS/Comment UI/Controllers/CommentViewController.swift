eba61e358874357fa0fd289629e4c22831d885d7
knn <nguyenkhoi2190@gmail.com>
2020-12-30 14:58:14 +0800

    Create CommentViewController and connect to story board

---
+++ EssentialFeed/EssentialFeediOS/Comment UI/Controllers/CommentViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CommentViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Khoi Nguyen on 30/12/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UITableViewController {
+	
+}
