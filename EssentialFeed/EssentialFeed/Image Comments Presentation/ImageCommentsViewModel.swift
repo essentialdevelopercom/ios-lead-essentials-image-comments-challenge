302e950f7b36186fec1771c6d2a0370a720109e6
Araceli Ruiz Ruiz <araruizalcuadrado@gmail.com>
2021-01-03 13:53:25 +0100

    Make 'ImagePresenter.map' private

--- EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsViewModel.swift
+++ EssentialFeed/EssentialFeed/Image Comments Presentation/ImageCommentsViewModel.swift
@@ -8,10 +8,20 @@
 
 public struct ImageCommentsViewModel {
     public let comments: [ImageCommentViewModel]
+	
+	public init(comments: [ImageCommentViewModel]) {
+		self.comments = comments
+	}
 }
 
 public struct ImageCommentViewModel: Equatable {
 	public let message: String
 	public let date: String
 	public let username: String
+	
+	public init(message: String, date: String, username: String) {
+		self.message = message
+		self.date = date
+		self.username = username
+	}
 }
