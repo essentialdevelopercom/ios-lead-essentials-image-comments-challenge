d4f885cd1d63cac77aec61dc70195efa67d7e98c
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-01-07 16:52:40 +0100

    Request data from URL on 'load'

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedImageCommentsFromRemoteUseCaseTests.swift
@@ -5,17 +5,46 @@
 import XCTest
 import EssentialFeed
 
-final class RemoteImageCommentsLoader {
-    init(client: HTTPClient) {
-        
+final class RemoteFeedImageCommentsLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url) { _ in}
     }
 }
 
 class LoadFeedImageCommentsFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotRequestFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteImageCommentsLoader(client: client)
+        _ = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_requestsFromURL() {
+        let url = anyURL()
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = anyURL(),file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageCommentsLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageCommentsLoader(url: url, client: client)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        
+        return (sut, client)
     }
 }
