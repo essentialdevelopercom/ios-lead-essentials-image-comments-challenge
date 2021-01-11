d974a1395c1779827a8afa594868dc776ea33545
Flavio Serrazes <flavio.serrazes@icloud.com>
2021-01-11 13:59:22 +0100

    Add localized 'FeedImageCommentsPresenter'

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageCommentsPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class FeedImageCommentsPresenter {
+
+     public static var title: String { NSLocalizedString(
+         "FEED_COMMENTS_VIEW_TITLE",
+         tableName: "FeedImageComments",
+         bundle: Bundle(for: FeedImageCommentsPresenter.self),
+         comment: "Title for the image comments view"
+     ) }
+ }
