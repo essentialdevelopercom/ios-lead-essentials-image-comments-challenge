2e30ac6958b90a53dc0d42719d163ab6095bc750
Robert Dates <rsndates@yahoo.com>
2021-01-19 20:54:52 -0500

    `RemoteCommentLoader` requests data from url

--- EssentialFeed/EssentialFeedTests/LoadCommentsFromRemoteUseCasesTests.swift
+++ EssentialFeed/EssentialFeedTests/LoadCommentsFromRemoteUseCasesTests.swift
@@ -10,8 +10,19 @@ import XCTest
 import EssentialFeed
 
 class RemoteFeedCommentsLoader {
-	init(client: HTTPClient) {
-		
+	
+	private let url: URL
+	private let client: HTTPClient
+	
+	init(client: HTTPClient, url: URL) {
+		self.url = url
+		self.client = client
+	}
+	
+	func load() {
+		client.get(from: url) { _ in
+			
+		}
 	}
 }
 
@@ -24,11 +35,20 @@ class LoadCommentsFromRemoteUseCasesTests: XCTestCase {
 		XCTAssertTrue(client.requestedURLs.isEmpty)
 	}
 	
+	func test_load_requestDataFromURL() {
+		let url = anyURL()
+		let (sut, client) = makeSUT(url: url)
+		
+		sut.load()
+		
+		XCTAssertEqual(client.requestedURLs, [url])
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(url: URL = anyURL(), file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedCommentsLoader, client: HTTPClientSpy) {
-		let client = HTTPClientSpy()
-		let sut = RemoteFeedCommentsLoader(client: client)
+		let client =  HTTPClientSpy()
+		let sut = RemoteFeedCommentsLoader(client: client, url: url)
 		trackForMemoryLeaks(client)
 		trackForMemoryLeaks(sut)
 		
