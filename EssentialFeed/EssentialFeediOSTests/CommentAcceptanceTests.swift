11dc4fde7c6efa67e130fb01ed7e37bcf7da92a5
Robert Dates <rsndates@yahoo.com>
2021-01-23 11:47:59 -0500

    Added `CommentAcceptanceTests` file

---
+++ EssentialFeed/EssentialFeediOSTests/CommentAcceptanceTests.swift
@@ -0,0 +1,16 @@
+//
+//  CommentAcceptanceTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Robert Dates on 1/23/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class CommentAcceptanceTests: XCTestCase {
+	
+}
