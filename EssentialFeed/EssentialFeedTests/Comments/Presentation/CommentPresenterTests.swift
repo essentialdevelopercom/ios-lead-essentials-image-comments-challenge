763631cddf3d5449e59c4a57a6416ab2595ff687
Robert Dates <rsndates@yahoo.com>
2021-01-23 13:06:17 -0500

    Added comments group in tests target and also added `CommentPresenterTests` file

---
+++ EssentialFeed/EssentialFeedTests/Comments/Presentation/CommentPresenterTests.swift
@@ -0,0 +1,13 @@
+//
+//  CommentPresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Robert Dates on 1/23/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+class CommentPresenterTests: XCTestCase {
+
+}
