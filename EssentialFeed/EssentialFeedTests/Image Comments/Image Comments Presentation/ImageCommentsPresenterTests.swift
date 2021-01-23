060573f688722eb1c35e7e78844bcc70eb3195e0
Lukas Bahrle <lukas.bahrle.santana@gmail.com>
2021-01-23 16:33:46 +0100

    extract SUT creation to centralised method and add memory leak tracking

--- EssentialFeed/EssentialFeedTests/Image Comments/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -26,13 +26,21 @@ class ImageCommentsPresenterTests: XCTestCase{
 	}
 	
 	func test_init_doesNotSendMessagesToView() {
-		let view = ViewSpy()
-		_ = ImageCommentsPresenter(imageCommentsView: view)
+		let (_, view) = makeSUT()
 		
 		XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
 	}
 	
+	
 	// MARK: - Helpers
+	
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageCommentsPresenter, view: ViewSpy) {
+		let view = ViewSpy()
+		let sut = ImageCommentsPresenter(imageCommentsView: view)
+		trackForMemoryLeaks(view, file: file, line: line)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return (sut, view)
+	}
 	
 	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
 		let table = "ImageComments"
