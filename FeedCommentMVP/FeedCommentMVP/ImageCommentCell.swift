a8ca253510b92326234b4c48b0837b03b6fb27c7
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-02-01 10:27:06 +0545

    Create 'ImageCommentCell' with storyBoard connections

---
+++ FeedCommentMVP/FeedCommentMVP/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  FeedCommentMVP
+//
+//  Created by Alok Subedi on 01/02/2021.
+//
+
+import UIKit
+
+class ImageCommentCell: UITableViewCell {
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var commentLabel: UILabel!
+}
