950efc853710314e7972d26edd3a207859b17899
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-02-01 10:10:10 +0545

    Make 'ViewController' render 10 cells

--- FeedCommentMVP/FeedCommentMVP/ViewController.swift
+++ FeedCommentMVP/FeedCommentMVP/ViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "ImageCommentCell")!
     }
 }
 
