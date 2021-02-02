cb9f3f05c2a72a734af928bebe263c50cebfd43d
aloksbd <30898579+aloksbd@users.noreply.github.com>
2021-02-02 11:47:44 +0545

    Move data mapping task in success case to separate class 'ImageCommentsMapper'

--- EssentialFeed/EssentialFeed/Image Comment/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Image Comment/RemoteImageCommentsLoader.swift
@@ -11,6 +11,38 @@ import Foundation
 public class RemoteImageCommentsLoader {
 	public typealias Result = Swift.Result<[ImageComment], Error>
 	
+	private let client: HTTPClient
+	private let url: URL
+	
+	public init(client: HTTPClient, url: URL) {
+		self.client = client
+		self.url = url
+	}
+	
+	public enum Error: Swift.Error {
+		case connectivity
+		case invalidData
+	}
+	
+	public func load(completion: @escaping (Result) -> Void) {
+		client.get(from: url) { [weak self] result  in
+			guard self != nil else { return }
+			switch result {
+			case let .success((data, response)):
+				do {
+					let comments = try ImageCommentsMapper.map(data: data, from: response)
+					completion(.success(comments))
+				} catch {
+					completion(.failure(error as! RemoteImageCommentsLoader.Error))
+				}
+			case .failure:
+				completion(.failure(.connectivity))
+			}
+		}
+	}
+}
+
+private class ImageCommentsMapper {
 	private struct Root: Decodable {
 		let items: [RemoteImageComment]
 		
@@ -30,39 +62,18 @@ public class RemoteImageCommentsLoader {
 		let username: String
 	}
 	
-	private let client: HTTPClient
-	private let url: URL
-	
-	public init(client: HTTPClient, url: URL) {
-		self.client = client
-		self.url = url
-	}
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-	
-	public func load(completion: @escaping (Result) -> Void) {
-		client.get(from: url) { [weak self] result  in
-			guard self != nil else { return }
-			switch result {
-			case let .success((data, response)):
-				if response.statusCode == 200 {
-					let jsonDecoder = JSONDecoder()
-					jsonDecoder.dateDecodingStrategy = .iso8601
-					do {
-					let root = try jsonDecoder.decode(Root.self, from: data)
-						completion(.success(root.imageComments))
-					} catch {
-						completion(.failure(.invalidData))
-					}
-				} else {
-					completion(.failure(.invalidData))
-				}
-			case .failure:
-				completion(.failure(.connectivity))
+	static func map(data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+		if response.statusCode == 200 {
+			let jsonDecoder = JSONDecoder()
+			jsonDecoder.dateDecodingStrategy = .iso8601
+			do {
+			let root = try jsonDecoder.decode(Root.self, from: data)
+				return root.imageComments
+			} catch {
+				throw RemoteImageCommentsLoader.Error.invalidData
 			}
+		} else {
+			throw RemoteImageCommentsLoader.Error.invalidData
 		}
 	}
 }
