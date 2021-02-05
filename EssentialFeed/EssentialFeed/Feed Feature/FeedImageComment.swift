aead7e90253f1e9c026a4781be08553c1285e36d
Mario Alberto Barragán Espinosa <>
2021-02-04 19:16:20 -0600

    Create the FeedImageComment model

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageComment.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageComment.swift
+//  EssentialFeed
+//
+//  Created by Mario Alberto Barragán Espinosa on 04/02/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct FeedImageComment {
+	let id: UUID
+	let message: String
+	let creationDate: Date
+	let author: CommentAuthor
+}
+
+struct CommentAuthor {
+	let username: String
+}
