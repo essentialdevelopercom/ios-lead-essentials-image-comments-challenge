c6b5fff2c993a1205bc4e61c4e301db6e65ceacb
Mario Alberto Barragán Espinosa <>
2021-02-07 18:41:45 -0600

    Create EssentialFeedAPIEndToEndTests test target

---
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Mario Alberto Barragán Espinosa on 07/02/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class EssentialFeedAPIEndToEndTests: XCTestCase {
+}
