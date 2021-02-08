fddc6c30340045eac5416faf25b67ebf39b26202
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2021-02-08 22:28:09 +0100

    Refactor tests to remove duplication

--- EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -24,21 +24,31 @@ public final class RemoteImageCommentsLoader {
 final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
 
 	func test_init_doesNotPerformURLRequest() {
-		let client = HTTPClientSpy()
-		let sut = RemoteImageCommentsLoader(client: client)
-
-		trackForMemoryLeaks(sut)
+		let (_, client) = makeSUT()
 
 		XCTAssertTrue(client.requestedURLs.isEmpty)
 	}
 
 	func test_load_requestsDataFromURL() {
-		let client = HTTPClientSpy()
-		let sut = RemoteImageCommentsLoader(client: client)
-
 		let url = URL(string: "https://a-given-url.com")!
+		let (sut, client) = makeSUT(url: url)
+
 		sut.load(from: url)
 
 		XCTAssertEqual(client.requestedURLs, [url])
+	}
+
+	// MARK: - Helpers
+
+	private func makeSUT(
+		url: URL = anyURL(),
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) -> (sut: RemoteImageCommentsLoader, client: HTTPClientSpy) {
+		let client = HTTPClientSpy()
+		let sut = RemoteImageCommentsLoader(client: client)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		trackForMemoryLeaks(client, file: file, line: line)
+		return (sut, client)
 	}
 }
