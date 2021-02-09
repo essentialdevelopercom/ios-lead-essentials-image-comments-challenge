d353e97ca1bcd4187ce80776f34b8568cd0276d0
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2021-02-10 00:01:26 +0100

    Move `RemoteImageCommentsLoader` and its collaborators to their own files

---
+++ EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
@@ -0,0 +1,79 @@
+//
+//  RemoteImageCommentsLoader.swift
+//  EssentialFeed
+//
+//  Created by Raphael Silva on 09/02/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class RemoteImageCommentsLoader {
+
+	public typealias Result = Swift.Result<[ImageComment], Swift.Error>
+
+	public enum Error: Swift.Error {
+		case connectivity
+		case invalidData
+	}
+
+	private final class HTTPClientTaskWrapper: HTTPClientTask {
+		private var completion: ((Result) -> Void)?
+		var wrapped: HTTPClientTask?
+
+		init(completion: @escaping (Result) -> Void) {
+			self.completion = completion
+		}
+
+		func cancel() {
+			wrapped?.cancel()
+			preventFurtherCompletions()
+		}
+
+		func complete(with result: Result) {
+			completion?(result)
+		}
+
+		private func preventFurtherCompletions() {
+			completion = nil
+		}
+	}
+
+	private let client: HTTPClient
+
+	public init(client: HTTPClient) {
+		self.client = client
+	}
+
+	@discardableResult
+	public func load(
+		from url: URL,
+		completion: @escaping (Result) -> Void
+	) -> HTTPClientTask {
+		let task = HTTPClientTaskWrapper(completion: completion)
+		task.wrapped = client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
+			switch result {
+			case let .success((data, response)):
+				task.complete(with: RemoteImageCommentsLoader.map(data, from: response))
+
+			case .failure:
+				task.complete(with: .failure(RemoteImageCommentsLoader.Error.connectivity))
+			}
+		}
+
+		return task
+	}
+
+	private static func map(
+		_ data: Data,
+		from response: HTTPURLResponse
+	) -> Result {
+		do {
+			let items = try ImageCommentsMapper.map(data, from: response)
+			return .success(items)
+		} catch {
+			return .failure(error)
+		}
+	}
+}
