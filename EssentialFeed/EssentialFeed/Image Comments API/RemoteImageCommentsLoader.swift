bd2da8d8a2d819f08563f541e06c08d8e423c045
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2021-02-11 21:17:55 +0100

    Replace `RemoteImageCommentsLoader` with `RemoteLoader`

--- EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentsLoader.swift
@@ -8,70 +8,17 @@
 
 import Foundation
 
-public final class RemoteImageCommentsLoader: ImageCommentsLoader {
+public typealias RemoteImageCommentsLoader = RemoteLoader<[ImageComment]>
 
-	public typealias Result = ImageCommentsLoader.Result
-
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-
-	private final class HTTPClientTaskWrapper: HTTPClientTask {
-		private var completion: ((Result) -> Void)?
-		var wrapped: HTTPClientTask?
-
-		init(completion: @escaping (Result) -> Void) {
-			self.completion = completion
-		}
-
-		func cancel() {
-			wrapped?.cancel()
-			preventFurtherCompletions()
-		}
-
-		func complete(with result: Result) {
-			completion?(result)
-		}
-
-		private func preventFurtherCompletions() {
-			completion = nil
-		}
-	}
-
-	private let url: URL
-	private let client: HTTPClient
-
-	public init(
+extension RemoteImageCommentsLoader {
+	public convenience init(
 		url: URL,
 		client: HTTPClient
 	) {
-		self.url = url
-		self.client = client
-	}
-
-	public func load(completion: @escaping (Result) -> Void) {
-		client.get(from: url) { [weak self] result in
-			guard self != nil else { return }
-			switch result {
-			case let .success((data, response)):
-				completion(RemoteImageCommentsLoader.map(data, from: response))
-
-			case .failure:
-				completion(.failure(Error.connectivity))
-			}
-		}
-	}
-
-	private static func map(
-		_ data: Data,
-		from response: HTTPURLResponse
-	) -> Result {
-		do {
-			let items = try ImageCommentsMapper.map(data, from: response)
-			return .success(items)
-		} catch {
-			return .failure(Error.invalidData)
-		}
+		self.init(
+			url: url,
+			client: client,
+			mapper: ImageCommentsMapper.map
+		)
 	}
 }
--- EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -11,44 +11,6 @@ import XCTest
 
 final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
 
-	func test_init_doesNotRequestDataFromURL() {
-		let (_, client) = makeSUT()
-
-		XCTAssertTrue(client.requestedURLs.isEmpty)
-	}
-
-	func test_load_requestsDataFromURL() {
-		let url = URL(string: "https://a-given-url.com")!
-		let (sut, client) = makeSUT(url: url)
-
-		sut.load { _ in }
-
-		XCTAssertEqual(client.requestedURLs, [url])
-	}
-
-	func test_loadTwice_requestsDataFromURLTwice() {
-		let url = URL(string: "https://a-given-url.com")!
-		let (sut, client) = makeSUT(url: url)
-
-		sut.load { _ in }
-		sut.load { _ in }
-
-		XCTAssertEqual(client.requestedURLs, [url, url])
-	}
-
-	func test_load_deliversErrorOnClientError() {
-		let (sut, client) = makeSUT()
-		let expectedError = RemoteImageCommentsLoader.Error.connectivity
-
-		expect(
-			sut: sut,
-			toCompleteWith: failure(expectedError),
-			when: {
-				client.complete(with: expectedError)
-			}
-		)
-	}
-
 	func test_load_deliversErrorOnNon2xxHTTPResponse() {
 		let (sut, client) = makeSUT()
 
