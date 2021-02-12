067f2c30821abad875dc642db134ecd08f8d71e3
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2021-02-12 23:15:14 +0100

    Replace `RemoteFeedLoader` with `RemoteLoader` and make `RemoteLoader` conform to `FeedLoader` in the Composition Root

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -9,11 +9,17 @@ final class FeedItemsMapper {
 		let items: [RemoteFeedItem]
 	}
 	
-	static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+	static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
 		guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			throw RemoteFeedLoader.Error.invalidData
 		}
 		
-		return root.items
+		return root.items.toModels()
+	}
+}
+
+private extension Array where Element == RemoteFeedItem {
+	func toModels() -> [FeedImage] {
+		return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image) }
 	}
 }
