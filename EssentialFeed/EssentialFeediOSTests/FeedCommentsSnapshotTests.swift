de44e239dd5df62cda861d8660ec8d8c491e0904
Mario Alberto Barragán Espinosa <>
2021-02-21 16:33:35 -0600

    FeedCommentsSnapshotTests sanpshots for not empty comments

--- EssentialFeed/EssentialFeediOSTests/FeedCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedCommentsSnapshotTests.swift
@@ -13,6 +13,15 @@ final class FeedCommentsSnapshotTests: XCTestCase {
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_COMMENTS_dark")
     }
 	
+	func test_feedCommentsWithContent() {
+		let sut = makeSUT()
+		
+		sut.display(feedCommentsWithContent())
+		
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_COMMENTS_WITH_CONTENT_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_COMMENTS_WITH_CONTENT_dark")
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT() -> FeedImageCommentViewController {
@@ -25,7 +34,40 @@ final class FeedCommentsSnapshotTests: XCTestCase {
 		return controller
 	}
 	
+	private func feedCommentsWithContent() -> [CommentStub] {
+		return [
+			CommentStub(message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.", 
+						authorName: "Mario", 
+						createdAt: "2 weeks ago"),
+			
+			CommentStub(message: "Hello world.", 
+						authorName: "Alberto", 
+						createdAt: "3 days ago")
+		]
+	}
+	
 	private func emptyComments() -> [FeedImageCommentCellController] {
 		return []
+	}
+}
+
+private extension FeedImageCommentViewController {
+	func display(_ stubs: [CommentStub]) {
+		let cells: [FeedImageCommentCellController] = stubs.map { stub in
+			let cellController = FeedImageCommentCellController(model: stub.viewModel)
+			return cellController
+		}
+		
+		display(cells)
+	}
+}
+
+private class CommentStub {
+	let viewModel: CommentItemViewModel
+	
+	init(message: String, authorName: String, createdAt: String) {
+		viewModel = CommentItemViewModel(message: message, 
+										 authorName: authorName, 
+										 createdAt: createdAt)
 	}
 }
