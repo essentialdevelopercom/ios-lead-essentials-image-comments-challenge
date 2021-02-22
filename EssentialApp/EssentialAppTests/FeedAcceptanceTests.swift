4abdffe056d5bf421123387941541912b349877e
Mario Alberto Barragán Espinosa <>
2021-02-21 18:26:46 -0600

    Move run loop to a helper method

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -57,7 +57,8 @@ class FeedAcceptanceTests: XCTestCase {
 		
 		feed.simulateFeedImageTap(at: 0)
 		
-		RunLoop.current.run(until: Date())
+		executeRunLoopToCleanUpReferences()
+		
 		let root = sut.window?.rootViewController
 		let rootNavigation = root as? UINavigationController
 		let topController = rootNavigation?.topViewController
@@ -148,5 +149,9 @@ class FeedAcceptanceTests: XCTestCase {
 				"author": ["username": "An Author"]
 			]
 		]])
+	}
+	
+	private func executeRunLoopToCleanUpReferences() {
+		RunLoop.current.run(until: Date())
 	}
 }
