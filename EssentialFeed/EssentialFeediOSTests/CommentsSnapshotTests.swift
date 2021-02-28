791270da4db9f17bea33b7e583d04cd7dbfddb14
Robert Dates <rsndates@yahoo.com>
2021-02-28 03:28:27 -0500

    Added error and fully loaded comment snapshot tests

--- EssentialFeed/EssentialFeediOSTests/CommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/CommentsSnapshotTests.swift
@@ -20,6 +20,24 @@ class CommentsSnapshotTests: XCTestCase {
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_IMAGE_COMMENTS_dark")
 	}
 	
+	func test_commentsWithErrorMessage() {
+		let sut = makeSUT()
+
+		sut.display(errorMessage("This is\na multi-line\nerror message"))
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "COMMENTS_WITH_ERROR_MESSAGE_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "COMMENTS_WITH_ERROR_MESSAGE_dark")
+	}
+	
+	func test_commentsWithContent() {
+		let sut = makeSUT()
+
+		sut.display(comments())
+
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "LOADED_COMMENTS_light")
+		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "LOADED_COMMENTS_dark")
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT() -> CommentsViewController {
@@ -36,6 +54,14 @@ class CommentsSnapshotTests: XCTestCase {
 
 	private func errorMessage(_ message: String) -> CommentErrorViewModel {
 		return CommentErrorViewModel(message: message)
+	}
+	
+	private func comments() -> CommentViewModel {
+		let comment0 = PresentableComment(username: "Some User", message: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr.", date: "1 year ago")
+		let comment1 = PresentableComment(username: "A very long long long username", message: "Lorem ipsum!\n.\n.\n.\n.", date: "6 month ago")
+		let comment2 = PresentableComment(username: "Another User", message: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.", date: "1 day ago")
+		let comment3 = PresentableComment(username: "Last User", message: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.", date: "Just now")
+		return CommentViewModel(comments: [comment0, comment1, comment2, comment3])
 	}
 	
 }
