a4c364ecd46b064d3ca284729ea3f196d7c24467
Danil <Danil.Vassyakin@kaspi.kz>
2021-03-01 20:37:39 +0600

    add  connectivity  error completion handler

--- EssentialFeed/EssentialFeedTests/Feed Comments/FeedImageCommentsTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Comments/FeedImageCommentsTests.swift
@@ -9,18 +9,25 @@
 import XCTest
 import EssentialFeed
 
-struct FeedComment {
+struct FeedComment: Hashable {
 	let id: UUID
 	let message: String
 	let createdAt: String
 	let author: FeedCommentAuthor
 }
 
-struct FeedCommentAuthor {
+struct FeedCommentAuthor: Hashable {
 	let username: String
 }
 
 class RemoteImageFeedCommentLoader {
+	
+	public enum Error: Swift.Error {
+		case connectivity
+		case invalidData
+	}
+	
+	typealias Result = Swift.Result<[FeedComment], Error>
 	
 	private let baseUrl: URL
 	private let client: HTTPClient
@@ -30,8 +37,14 @@ class RemoteImageFeedCommentLoader {
 		self.client = client
 	}
 	
-	func load(imageId: String, completion: @escaping (Result<[FeedComment], Error>) -> Void) {
-		client.get(from: baseUrl, completion: { _ in })
+	func load(imageId: String, completion: @escaping (Result) -> Void) {
+		client.get(from: baseUrl, completion: { result in
+			switch result {
+			case .success: break
+			case .failure:
+				completion(.failure(.connectivity))
+			}
+		})
 	}
 	
 }
@@ -53,12 +66,48 @@ final class FeedImageCommentsTests: XCTestCase {
 		XCTAssertEqual(client.requestedURLs, [url])
 	}
 	
+	func test_load_deliversErrorOnClientError() {
+		let (sut, client) = makeSUT()
+		
+		expect(sut, toCompleteWith: failure(.connectivity), when: {
+			let clientError = NSError(domain: "Test", code: 0)
+			client.complete(with: clientError)
+		})
+	}
+	
+	private func failure(_ error: RemoteImageFeedCommentLoader.Error) -> RemoteImageFeedCommentLoader.Result {
+		return .failure(error)
+	}
+	
 	private func makeSUT(url: URL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!) -> (RemoteImageFeedCommentLoader, HTTPClientSpy) {
 		let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!
 		let client = HTTPClientSpy()
 		let loader = RemoteImageFeedCommentLoader(baseUrl: url, client: client)
 
 		return (loader, client)
+	}
+	
+	private func expect(_ sut: RemoteImageFeedCommentLoader, toCompleteWith expectedResult: RemoteImageFeedCommentLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+		let exp = expectation(description: "Wait for load completion")
+		
+		sut.load(imageId: "any") { receivedResult in
+			switch (receivedResult, expectedResult) {
+			case let (.success(receivedItems), .success(expectedItems)):
+				XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+				
+			case let (.failure(receivedError), .failure(expectedError)):
+				XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+				
+			default:
+				XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+			}
+			
+			exp.fulfill()
+		}
+		
+		action()
+		
+		wait(for: [exp], timeout: 1.0)
 	}
 	
 }
