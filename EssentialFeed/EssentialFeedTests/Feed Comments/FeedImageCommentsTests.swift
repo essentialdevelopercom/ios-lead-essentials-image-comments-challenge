8659e49a9ddddddfb9f2ea7c8fd03974755fa2ce
Danil <Danil.Vassyakin@kaspi.kz>
2021-03-01 19:52:55 +0600

    refactored; add tests to check invoking load method for given url

--- EssentialFeed/EssentialFeedTests/Feed Comments/FeedImageCommentsTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Comments/FeedImageCommentsTests.swift
@@ -22,14 +22,16 @@ struct FeedCommentAuthor {
 
 class RemoteImageFeedCommentLoader {
 	
+	private let baseUrl: URL
 	private let client: HTTPClient
 	
-	init(client: HTTPClient) {
+	init(baseUrl: URL, client: HTTPClient) {
+		self.baseUrl = baseUrl
 		self.client = client
 	}
 	
 	func load(imageId: String, completion: @escaping (Result<[FeedComment], Error>) -> Void) {
-		
+		client.get(from: baseUrl, completion: { _ in })
 	}
 	
 }
@@ -37,12 +39,26 @@ class RemoteImageFeedCommentLoader {
 final class FeedImageCommentsTests: XCTestCase {
 
 	func test_init_doesNotRequestDataFromUrl() {
-		let client = HTTPClientSpy()
-		let loader = RemoteImageFeedCommentLoader(client: client)
+		let (_, client) = makeSUT()
 		
-		loader.load(imageId: "any", completion: { _ in })
 		XCTAssertTrue(client.requestedURLs.isEmpty)
 	}
 	
+	func test_load_requestsDataFromURL() {
+		let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!
+		let (loader, client) = makeSUT(url: url)
+
+		loader.load(imageId: "any", completion: { _ in })
+
+		XCTAssertEqual(client.requestedURLs, [url])
+	}
+	
+	private func makeSUT(url: URL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!) -> (RemoteImageFeedCommentLoader, HTTPClientSpy) {
+		let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!
+		let client = HTTPClientSpy()
+		let loader = RemoteImageFeedCommentLoader(baseUrl: url, client: client)
+
+		return (loader, client)
+	}
 	
 }
