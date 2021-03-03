1ef8f0fb73cf15426495bc8e0d5a7c36ade8d4d9
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-03-03 12:02:16 +0300

    Proves that load method multiple calls invokes URL loading the same amount of times

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedImageCommentsFromRemoteUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedImageCommentsFromRemoteUseCase.swift
@@ -40,9 +40,19 @@ class LoadFeedImageCommentsFromRemoteUseCase: XCTestCase {
 		XCTAssertEqual(client.requestedURLs, [url])
 	}
 	
+	func test_loadTwice_requestsDataFromURLTwice() {
+		let url = URL(string: "http://a-url.com")!
+		let (sut, client) = makeSUT(url: url)
+		
+		sut.load()
+		sut.load()
+		
+		XCTAssertEqual(client.requestedURLs, [url, url])
+	}
+	
 	// MARK - Helpers
 	
-	func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (RemoteFeedImageCommentsLoader, HTTPClientSpy) {
+	func makeSUT(url: URL = URL(string: "https://a-default-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (RemoteFeedImageCommentsLoader, HTTPClientSpy) {
 		let client = HTTPClientSpy()
 		let sut = RemoteFeedImageCommentsLoader(client: client, url: url)
 		return (sut, client)
