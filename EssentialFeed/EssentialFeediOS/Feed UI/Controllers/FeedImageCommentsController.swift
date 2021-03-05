c2f64c98929db5ae967d8199adaa8332e4ce6e2d
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-03-05 19:04:16 +0300

    Add simple image comments view controller prototype

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCommentsController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageCommentsController.swift
+//  EssentialFeediOS
+//
+//  Created by Anton Ilinykh on 05.03.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+class FeedImageCommentsController: UITableViewController {
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 10
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return tableView.dequeueReusableCell(withIdentifier: "FeedImageCommentCell")!
+	}
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/FeedImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Anton Ilinykh on 05.03.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCommentCell: UITableViewCell {
+	@IBOutlet private(set) public var authorLabel: UILabel!
+	@IBOutlet private(set) public var dateLabel: UILabel!
+	@IBOutlet private(set) public var commentLabel: UILabel!
+}
