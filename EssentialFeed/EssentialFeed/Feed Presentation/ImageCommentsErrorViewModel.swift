eedb69eedd25864e2e5a206ef3b72f52033d1715
Adrian Szymanowski <adriano100417@gmail.com>
2021-03-08 11:39:02 +0100

    Move `ImageCommentsErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/ImageCommentsErrorViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCommentsErrorViewModel.swift
+//  EssentialFeed
+//
+//  Created by Adrian Szymanowski on 08/03/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public struct ImageCommentsErrorViewModel {
+	public let message: String?
+}
