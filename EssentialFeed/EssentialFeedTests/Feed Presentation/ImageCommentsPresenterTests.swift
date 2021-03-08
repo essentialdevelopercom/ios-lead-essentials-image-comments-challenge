efb9ada1ead3200d2c3925120cb62c6cc38e2817
Adrian Szymanowski <adriano100417@gmail.com>
2021-03-08 09:48:31 +0100

    `imageCommentsPresenter` successfully localize the title for the view

--- EssentialFeed/EssentialFeedTests/Feed Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/ImageCommentsPresenterTests.swift
@@ -58,6 +58,14 @@ class ImageCommentsPresenter {
 		errorView.display(ImageCommentsErrorViewModel(message: imageCommentsLoadError))
 		loadingView.display(ImageCommentsLoadingViewModel(isLoading: false))
 	}
+	
+	static var title: String {
+		NSLocalizedString(
+			"IMAGE_COMMENTS_VIEW_TITLE",
+			tableName: "ImageComments",
+			bundle: Bundle(for: ImageCommentsPresenter.self),
+			comment: "Title for the image comments view")
+	}
 }
 
 class ImageCommentsPresenterTests: XCTestCase {
@@ -102,6 +110,10 @@ class ImageCommentsPresenterTests: XCTestCase {
 		])
 	}
 	
+	func test_title_isLocalized() {
+		XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: ImageCommentsPresenter, view: ViewSpy) {
@@ -126,6 +138,16 @@ class ImageCommentsPresenterTests: XCTestCase {
 			uniqueComment(),
 			uniqueComment()
 		]
+	}
+	
+	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+		let table = "ImageComments"
+		let bundle = Bundle(for: ImageCommentsPresenter.self)
+		let value = bundle.localizedString(forKey: key, value: nil, table: table)
+		if value == key {
+			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+		}
+		return value
 	}
 
 	private class ViewSpy: ImageCommentsView, ImageCommentsLoadingView, ImageCommentsErrorView {
