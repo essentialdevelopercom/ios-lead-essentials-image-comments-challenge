a8ad6f4909d083ff1164d9db467da20b2abd4d72
Eric Garlock <edg234@gmail.com>
2021-03-09 07:03:51 -0600

    Added ImageCommentViewModel to hold actual display values including formatted time since comment createdAt

--- EssentialFeed/EssentialFeedTests/ImageComments Presentation/ImageCommentPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageComments Presentation/ImageCommentPresenterTests.swift
@@ -9,8 +9,14 @@
 import XCTest
 import EssentialFeed
 
-struct ImageCommentViewModel {
-	public let comments: [ImageComment]
+struct ImageCommentViewModel: Hashable {
+	public let message: String
+	public let created: String
+	public let username: String
+}
+
+struct ImageCommentsViewModel {
+	public let comments: [ImageCommentViewModel]
 }
 
 struct ImageCommentLoadingViewModel {
@@ -33,7 +39,7 @@ struct ImageCommentErrorViewModel {
 }
 
 protocol ImageCommentView {
-	func display(_ viewModel: ImageCommentViewModel)
+	func display(_ viewModel: ImageCommentsViewModel)
 }
 
 protocol ImageCommentLoadingView {
@@ -58,10 +64,16 @@ class ImageCommentPresenter {
 	private let loadingView: ImageCommentLoadingView
 	private let errorView: ImageCommentErrorView
 	
-	public init(commentView: ImageCommentView, loadingView: ImageCommentLoadingView, errorView: ImageCommentErrorView) {
+	private let currentDate: () -> Date
+	private let locale: Locale
+	private let calendar = Calendar(identifier: .gregorian)
+	
+	public init(commentView: ImageCommentView, loadingView: ImageCommentLoadingView, errorView: ImageCommentErrorView, currentDate: @escaping () -> Date = Date.init, locale: Locale = .current) {
 		self.commentView = commentView
 		self.loadingView = loadingView
 		self.errorView = errorView
+		self.currentDate = currentDate
+		self.locale = locale
 	}
 	
 	public func didStartLoadingComments() {
@@ -71,12 +83,22 @@ class ImageCommentPresenter {
 	
 	public func didFinishLoadingComments(with comments: [ImageComment]) {
 		loadingView.display(.finished)
-		commentView.display(ImageCommentViewModel(comments: comments))
+		commentView.display(ImageCommentsViewModel(comments: comments.map { comment in
+			ImageCommentViewModel(message: comment.message, created: formatted(since: comment.createdAt), username: comment.author.username)
+		}))
 	}
 	
 	public func didFinishLoadingComments(with error: Error) {
 		loadingView.display(.finished)
 		errorView.display(ImageCommentErrorViewModel(message: ImageCommentPresenter.errorMessage))
+	}
+	
+	private func formatted(since date: Date) -> String {
+		let formatter = RelativeDateTimeFormatter()
+		formatter.unitsStyle = .full
+		formatter.locale = locale
+		formatter.calendar = calendar
+		return formatter.localizedString(for: date, relativeTo: currentDate())
 	}
 	
 }
@@ -105,15 +127,30 @@ class ImageCommentPresenterTests: XCTestCase {
 	}
 	
 	func test_didFinishLoadingComments_displaysCommentsAndStopsLoading() {
-		let (sut, view) = makeSUT()
-		let comment0 = makeComment(id: UUID(), message: "message0", createdAt: Date(), username: "username0")
-		let comment1 = makeComment(id: UUID(), message: "message1", createdAt: Date(), username: "username1")
+		let fixedDate = Date()
+		let (sut, view) = makeSUT(currentDate: { fixedDate }, locale: .current)
 		
-		sut.didFinishLoadingComments(with: [comment0, comment1])
+		let comments = [
+			makeComment(id: UUID(), message: "message0", createdAt: fixedDate.adding(seconds: -30), username: "username0"),
+			makeComment(id: UUID(), message: "message1", createdAt: fixedDate.adding(seconds: -30 * 60), username: "username1"),
+			makeComment(id: UUID(), message: "message2", createdAt: fixedDate.adding(days: -1), username: "username2"),
+			makeComment(id: UUID(), message: "message3", createdAt: fixedDate.adding(days: -2), username: "username3"),
+			makeComment(id: UUID(), message: "message4", createdAt: fixedDate.adding(days: -7), username: "username4")
+		]
+		
+		let commentViewModels = [
+			ImageCommentViewModel(message: "message0", created: "30 seconds ago", username: "username0"),
+			ImageCommentViewModel(message: "message1", created: "30 minutes ago", username: "username1"),
+			ImageCommentViewModel(message: "message2", created: "1 day ago", username: "username2"),
+			ImageCommentViewModel(message: "message3", created: "2 days ago", username: "username3"),
+			ImageCommentViewModel(message: "message4", created: "1 week ago", username: "username4")
+		]
+		
+		sut.didFinishLoadingComments(with: comments)
 		
 		XCTAssertEqual(view.messages, [
 			.display(isLoading: false),
-			.display(comments: [comment0, comment1])
+			.display(comments: commentViewModels)
 		])
 	}
 	
@@ -130,9 +167,9 @@ class ImageCommentPresenterTests: XCTestCase {
 	}
 	
 	// MARK: - Helpers
-	private func makeSUT() -> (sut: ImageCommentPresenter, view: ViewSpy) {
+	private func makeSUT(currentDate: @escaping () -> Date = Date.init, locale: Locale = .current) -> (sut: ImageCommentPresenter, view: ViewSpy) {
 		let view = ViewSpy()
-		let sut = ImageCommentPresenter(commentView: view, loadingView: view, errorView: view)
+		let sut = ImageCommentPresenter(commentView: view, loadingView: view, errorView: view, currentDate: currentDate, locale: locale)
 		trackForMemoryLeaks(view)
 		trackForMemoryLeaks(sut)
 		return (sut, view)
@@ -159,14 +196,14 @@ class ImageCommentPresenterTests: XCTestCase {
 	private class ViewSpy: ImageCommentView, ImageCommentLoadingView, ImageCommentErrorView {
 		
 		enum Message: Hashable {
-			case display(comments: [ImageComment])
+			case display(comments: [ImageCommentViewModel])
 			case display(isLoading: Bool)
 			case display(message: String?)
 		}
 		
 		private(set) var messages = Set<Message>()
 		
-		func display(_ viewModel: ImageCommentViewModel) {
+		func display(_ viewModel: ImageCommentsViewModel) {
 			messages.insert(.display(comments: viewModel.comments))
 		}
 		
@@ -179,5 +216,5 @@ class ImageCommentPresenterTests: XCTestCase {
 		}
 		
 	}
-
+	
 }
