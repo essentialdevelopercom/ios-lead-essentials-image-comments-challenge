9046cb94beb5396e84ca27cd15ea3292d18e8e14
Adrian Szymanowski <adriano100417@gmail.com>
2021-03-09 17:09:11 +0100

    Add `ImageComments` view in storyboard with empty abstractions of `ImageCommentsViewController` and `ImageCommentCell`

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/ImageCommentsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCommentsViewController.swift
+//  EssentialFeediOS
+//
+//  Created by Adrian Szymanowski on 08/03/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class ImageCommentCell: UITableViewCell {
+	
+}
+
+public final class ImageCommentsViewController: UITableViewController {
+	
+}
