e4742abd7abea84d154d892dc8334919963e9fef
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-03-10 23:26:21 +0300

    Move `MainQueueDispatchDecorator` into a separate file

--- EssentialFeed/EssentialFeediOS/Feed UI/CommentsUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/CommentsUIComposer.swift
@@ -45,30 +45,6 @@ extension WeakRefVirtualProxy: CommentLoadingView where T: CommentLoadingView {
 	}
 }
 
-private final class MainQueueDispatchDecorator<T> {
-	private let decoratee: T
-	
-	init(decoratee: T) {
-		self.decoratee = decoratee
-	}
-	
-	private func dispatch(completion: @escaping () -> Void) {
-		guard Thread.isMainThread else {
-			return DispatchQueue.main.async(execute: completion)
-		}
-		
-		completion()
-	}
-}
-
-extension MainQueueDispatchDecorator: CommentsLoader where T == CommentsLoader {
-	func load(completion: @escaping (CommentsLoader.Result) -> Void) {
-		decoratee.load { [weak self] result in
-			self?.dispatch { completion(result) }
-		}
-	}
-}
-
 private final class CommentsAdapter: CommentView {
 	private weak var controller: CommentsController?
 	
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/MainQueueDispatchDecorator.swift
@@ -0,0 +1,34 @@
+//
+//  MainQueueDispatchDecorator.swift
+//  EssentialFeediOS
+//
+//  Created by Anton Ilinykh on 10.03.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+final class MainQueueDispatchDecorator<T> {
+	private let decoratee: T
+	
+	init(decoratee: T) {
+		self.decoratee = decoratee
+	}
+	
+	private func dispatch(completion: @escaping () -> Void) {
+		guard Thread.isMainThread else {
+			return DispatchQueue.main.async(execute: completion)
+		}
+		
+		completion()
+	}
+}
+
+extension MainQueueDispatchDecorator: CommentsLoader where T == CommentsLoader {
+	func load(completion: @escaping (CommentsLoader.Result) -> Void) {
+		decoratee.load { [weak self] result in
+			self?.dispatch { completion(result) }
+		}
+	}
+}
