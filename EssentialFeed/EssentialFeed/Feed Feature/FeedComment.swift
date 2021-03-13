3290f5c74f9b6fcc1af9ae84f5b49352028d67e7
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-03-13 20:57:19 +0500

    Add comments loader protocol and FeedComment

--- EssentialFeed/EssentialFeed/Feed Feature/FeedComment.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedComment.swift
@@ -1,9 +1,13 @@
 //
-//  FeedComment.swift
-//  EssentialFeed
-//
 //  Created by Azamat Valitov on 13.03.2021.
 //  Copyright © 2021 Essential Developer. All rights reserved.
 //
 
 import Foundation
+
+struct FeedComment {
+	let id: UUID
+	let message: String
+	let date: Date
+	let authorName: String
+}
--- EssentialFeed/EssentialFeed/Feed Feature/FeedCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCommentsLoader.swift
@@ -1,9 +1,12 @@
 //
-//  FeedCommentsLoader.swift
-//  EssentialFeed
-//
 //  Created by Azamat Valitov on 13.03.2021.
 //  Copyright © 2021 Essential Developer. All rights reserved.
 //
 
 import Foundation
+
+protocol FeedCommentsLoader {
+	typealias Result = Swift.Result<[FeedComment], Error>
+	
+	func load(completion: @escaping (Result) -> Void)
+}
