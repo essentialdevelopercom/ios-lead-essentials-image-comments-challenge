9e6e890f132576f31dff0710017c093fb37d4c8d
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-03-13 20:56:05 +0500

    Remove Team from signing optionsto to run tests on macos target

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCommentsLoader.swift
@@ -0,0 +1,9 @@
+//
+//  FeedCommentsLoader.swift
+//  EssentialFeed
+//
+//  Created by Azamat Valitov on 13.03.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
