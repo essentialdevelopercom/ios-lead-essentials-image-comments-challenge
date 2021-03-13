93d6babae300b016b9d76252a85f28c24a9b5469
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-03-13 23:28:57 +0500

    Move  to production

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedCommentsFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedCommentsFromRemoteUseCaseTests.swift
@@ -6,52 +6,6 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedCommentsLoader {
-	
-	public enum Error: Swift.Error {
-		case connectivity
-		case invalidData
-	}
-	
-	private let client: HTTPClient
-	init(client: HTTPClient) {
-		self.client = client
-	}
-	
-	func load(url: URL, completion: @escaping (Result<[FeedComment], Error>)->()) {
-		client.get(from: url, completion: {[weak self] result in
-			guard let _ = self else { return }
-			switch result {
-			case .success(let (data, response)):
-				let decoder = JSONDecoder()
-				decoder.dateDecodingStrategy = .iso8601
-				if response.statusCode == 200, let root = try? decoder.decode(Root.self, from: data) {
-					completion(.success(root.items.map({FeedComment(id: $0.id, message: $0.message, date: $0.created_at, authorName: $0.author.username)})))
-				}else{
-					completion(.failure(.invalidData))
-				}
-			case .failure:
-				completion(.failure(.connectivity))
-			}
-		})
-	}
-}
-
-private struct Root: Decodable {
-	let items: [RemoteFeedComment]
-}
-
-private struct RemoteFeedComment: Decodable {
-	let id: UUID
-	let message: String
-	let created_at: Date
-	let author: RemoteAuthor
-}
-
-private struct RemoteAuthor: Decodable {
-	let username: String
-}
-
 class LoadFeedCommentsFromRemoteUseCaseTests: XCTestCase {
 	
 	func test_init_doesNotRequestData() {
