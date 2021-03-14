576d241504809433b049ed4ce14ae7039924d260
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-03-14 15:48:57 +0500

    Add feed comments UI integration tests

---
+++ EssentialApp/EssentialAppTests/FeedCommentsUIIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Azamat Valitov on 14.03.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+class FeedCommentsUIIntegrationTests: XCTestCase {
+	func test() {
+		
+	}
+}
