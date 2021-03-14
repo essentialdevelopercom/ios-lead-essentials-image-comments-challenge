2e1a84ceef4389dc9fe95440cc8a1b26bb196357
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-03-14 15:59:20 +0500

    Feed comments view has title

--- EssentialApp/EssentialAppTests/FeedCommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedCommentsUIIntegrationTests.swift
@@ -5,8 +5,38 @@
 
 import XCTest
 
+class FeedCommentsViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = feedCommentsTitle
+	}
+	
+	private var feedCommentsTitle: String {
+		return NSLocalizedString("FEED_COMMENTS_VIEW_TITLE",
+			 tableName: "FeedComments",
+			 bundle: Bundle(for: FeedCommentsViewController.self),
+			 comment: "Title for feed comments view")
+	}
+}
+
 class FeedCommentsUIIntegrationTests: XCTestCase {
-	func test() {
+	func test_feedCommentsView_hasTitle() {
+		let sut = FeedCommentsViewController()
 		
+		sut.loadViewIfNeeded()
+		
+		XCTAssertEqual(sut.title, localized("FEED_COMMENTS_VIEW_TITLE"))
+	}
+	
+	// MARK: - Helpers
+	
+	func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+		let table = "FeedComments"
+		let bundle = Bundle(for: FeedCommentsViewController.self)
+		let value = bundle.localizedString(forKey: key, value: nil, table: table)
+		if value == key {
+			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+		}
+		return value
 	}
 }
