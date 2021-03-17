960be62c3ce32ab1e8b70a3e99ab060325b3a35f
Adrian Szymanowski <adriano100417@gmail.com>
2021-03-17 15:21:57 +0100

    ImageCommentsView on appear displays remote image comments when customer has connectivity

---
+++ EssentialApp/EssentialAppTests/ImageCommentsAcceptanceTests.swift
@@ -0,0 +1,56 @@
+//
+//  ImageCommentsAcceptanceTests.swift
+//  EssentialAppTests
+//
+//  Created by Adrian Szymanowski on 17/03/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+import EssentialFeediOS
+@testable import EssentialApp
+
+class ImageCommentsAcceptanceTests: XCTestCase {
+	
+	func test_onAppear_displaysRemoteImageCommentsWhenCustomerHasConnectivity() {
+		let comments = launch(httpClient: .online(response))
+		
+		XCTAssertEqual(comments.numberOfRenderedImageCommentsViews(), 2)
+	}
+	
+	// MARK: - Helpers
+	
+	private func launch(httpClient: HTTPClientStub = .offline) -> ImageCommentsViewController {
+		let store = InMemoryFeedStore.empty
+		let sut = SceneDelegate(httpClient: httpClient, store: store)
+		sut.window = UIWindow()
+		sut.configureWindow()
+		sut.openCommentsWithId(anyID)
+		RunLoop.main.run(until: Date())
+		
+		let nav = sut.window?.rootViewController as? UINavigationController
+		return nav?.topViewController as! ImageCommentsViewController
+	}
+	
+	private func response(for url: URL) -> (Data, HTTPURLResponse) {
+		let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+		return (makeData(for: url), response)
+	}
+	
+	private func makeData(for url: URL) -> Data {
+		makeImageCommentsData()
+	}
+	
+	private var anyID: String {
+		"77a9c2d5-5e77-45d5-bf7b-4d17b0ab3321"
+	}
+	
+	private func makeImageCommentsData() -> Data {
+		return try! JSONSerialization.data(withJSONObject: ["items": [
+			["id": "d7e5515d-8dd4-441f-9872-530768d9f3e9", "message": "test message", "created_at" : "2014-12-24T00:00:00+00:00", "author": ["username": "test user"]],
+			["id": "1fb87ef8-c25b-42b8-8ff1-ffc6b32b1687", "message": "test message 2", "created_at" : "2021-01-02T12:13:14+00:00", "author": ["username": "test user 2"]]
+		]])
+	}
+	
+}
