a0fa1e1b7e40cc659c9795f125376095b5be4a45
Adrian Szymanowski <adriano100417@gmail.com>
2021-03-17 14:52:52 +0100

    Feed image selection calls navigation action to show image comments view

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -52,6 +52,15 @@ class FeedAcceptanceTests: XCTestCase {
 		XCTAssertNotNil(store.feedCache, "Expected to keep non-expired cache")
 	}
 	
+	func test_feedImageSelect_callsNavigationActionToImageComments() {
+		let feed = launch(httpClient: .online(response), store: .empty)
+		
+		feed.simulateFeedImageSelection(at: 0)
+		forceUIUpdate()
+		
+		XCTAssertNotNil(feed.navigationController?.topViewController as? ImageCommentsViewController, "Expected `ImageViewController` to be shown on the top of view hierarchy")
+	}
+	
 	// MARK: - Helpers
 	
 	private func launch(
@@ -76,10 +85,21 @@ class FeedAcceptanceTests: XCTestCase {
 		return (makeData(for: url), response)
 	}
 	
+	/// Adds a overhead of 100 miliseconds but gives enough time for distributing notification of state change in UITapGestureRecognizer
+	private func forceUIUpdate() {
+		RunLoop.main.run(until: Date().addingTimeInterval(0.1))
+	}
+	
 	private func makeData(for url: URL) -> Data {
 		switch url.absoluteString {
 		case "http://image.com":
 			return makeImageData()
+			
+		case "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed":
+			return makeFeedData()
+			
+		case "https://ile-api.essentialdeveloper.com/essential-feed/v1/image/e8ab0ee8-8728-11eb-8dcd-0242ac130003/comments":
+			return Data()
 			
 		default:
 			return makeFeedData()
@@ -92,8 +112,8 @@ class FeedAcceptanceTests: XCTestCase {
 	
 	private func makeFeedData() -> Data {
 		return try! JSONSerialization.data(withJSONObject: ["items": [
-			["id": UUID().uuidString, "image": "http://image.com"],
-			["id": UUID().uuidString, "image": "http://image.com"]
+			["id": "e8ab0ee8-8728-11eb-8dcd-0242ac130003", "image": "http://image.com"],
+			["id": "867c7e6b-0031-49a3-b07d-37cf811fc856", "image": "http://image.com"]
 		]])
 	}
 	
