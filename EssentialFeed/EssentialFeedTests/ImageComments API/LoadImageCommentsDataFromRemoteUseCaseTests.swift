2cf6672dce5bca0151393a6db204a4a989ac96a7
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-03-19 22:31:25 +0200

    add LoadImageCommentsDataFromRemoteUseCaseTests class

---
+++ EssentialFeed/EssentialFeedTests/ImageComments API/LoadImageCommentsDataFromRemoteUseCaseTests.swift
@@ -0,0 +1,15 @@
+//
+//  LoadImageCommentsDataFromRemoteUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Sebastian Vidrea on 19.03.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+class LoadImageCommentsDataFromRemoteUseCaseTests: XCTestCase {
+
+	
+
+}
