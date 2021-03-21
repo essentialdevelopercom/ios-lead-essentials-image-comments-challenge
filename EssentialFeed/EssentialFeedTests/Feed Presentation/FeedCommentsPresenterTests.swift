314ced659ca9bd85721170ab6982a9b17a32a219
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-03-21 18:23:33 +0500

    FeedCommentsPresenter's title is localized

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedCommentsPresenterTests.swift
@@ -7,13 +7,29 @@ import XCTest
 import EssentialFeed
 
 class FeedCommentsPresenter {
-	
+	public static var title: String {
+		return NSLocalizedString("FEED_COMMENTS_VIEW_TITLE",
+			 tableName: "FeedComments",
+			 bundle: Bundle(for: FeedCommentsPresenter.self),
+			 comment: "Title for feed comments view")
+	}
 }
 
 class FeedCommentsPresenterTests: XCTestCase {
 	
-	func test() {
-		let sut = FeedCommentsPresenter()
+	func test_title_isLocalized() {
+		XCTAssertEqual(FeedCommentsPresenter.title, localized("FEED_COMMENTS_VIEW_TITLE"))
 	}
 	
+	// MARK: - Helpers
+	
+	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
+		let table = "FeedComments"
+		let bundle = Bundle(for: FeedCommentsPresenter.self)
+		let value = bundle.localizedString(forKey: key, value: nil, table: table)
+		if value == key {
+			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+		}
+		return value
+	}
 }
