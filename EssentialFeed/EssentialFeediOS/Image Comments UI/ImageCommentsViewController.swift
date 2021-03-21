6514e128b4609f6d0b54f93e0d24338afe552ff0
Ángel Vázquez <angel_betta@outlook.com>
2021-03-21 15:41:12 -0600

    Render comments list on successful load completion with relative date formatting

--- EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsViewController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsViewController.swift
@@ -9,13 +9,23 @@
 import EssentialFeed
 import UIKit
 
+public class ImageCommentCell: UITableViewCell {
+	public let authorLabel = UILabel()
+	public let creationDateLabel = UILabel()
+	public let messageLabel = UILabel()
+}
+
 public class ImageCommentsViewController: UITableViewController {
 	private var url: URL!
+	private var currentDate: (() -> Date)!
 	private var loader: ImageCommentLoader?
 	
-	public convenience init(url: URL, loader: ImageCommentLoader) {
+	private var tableModel = [ImageComment]()
+	
+	public convenience init(url: URL, currentDate: @escaping () -> Date, loader: ImageCommentLoader) {
 		self.init()
 		self.url = url
+		self.currentDate = currentDate
 		self.loader = loader
 	}
 	
@@ -30,9 +40,29 @@ public class ImageCommentsViewController: UITableViewController {
 	
 	@objc private func load() {
 		refreshControl?.beginRefreshing()
-		_ = loader?.load(from: url) { [weak self] _ in
+		_ = loader?.load(from: url) { [weak self] result in
+			self?.tableModel = (try? result.get()) ?? []
+			self?.tableView.reloadData()
 			self?.refreshControl?.endRefreshing()
 		}
+	}
+	
+	public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		tableModel.count
+	}
+	
+	public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cellModel = tableModel[indexPath.row]
+		let cell = ImageCommentCell()
+		cell.authorLabel.text = cellModel.author
+		cell.messageLabel.text = cellModel.message
+		cell.creationDateLabel.text = formatRelativeDate(for: cellModel.creationDate)
+		return cell
+	}
+	
+	private func formatRelativeDate(for date: Date) -> String {
+		let formatter = RelativeDateTimeFormatter()
+		return formatter.localizedString(for: date, relativeTo: currentDate())
 	}
 }
 
--- EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
@@ -28,8 +28,7 @@ class ImageCommentsViewControllerTests: XCTestCase {
 	}
 	
 	func test_loadingSpinner_isVisibleWhileLoadingComments() {
-		let url = URL(string: "https://any-url.com")!
-		let (sut, loader) = makeSUT(url: url)
+		let (sut, loader) = makeSUT(url: anyURL())
 		
 		sut.loadViewIfNeeded()
 		XCTAssertTrue(sut.isShowingLoadingSpinner, "Expected loading spinner to be shown when view has loaded")
@@ -44,14 +43,110 @@ class ImageCommentsViewControllerTests: XCTestCase {
 		XCTAssertFalse(sut.isShowingLoadingSpinner, "Expected loading spinner to stop animating upon loader completion")
 	}
 	
+	func test_loadCommentsCompletion_rendersSuccessfullyLoadedComments() {
+		let staticDate = makeDateFromTimestamp(1_605_868_247, description: "2020-11-20 10:30:47 +0000")
+		
+		let comment0 = makeComment(
+			message: "a message",
+			creationDate: makeDateFromTimestamp(1_605_860_313, description: "2020-11-20 08:18:33 +0000"),
+			author: "an author"
+		)
+		let expectedRelativeDate0 = "2 hours ago"
+		
+		let comment1 = makeComment(
+			message: "another message",
+			creationDate: makeDateFromTimestamp(1_605_713_544, description: "2020-11-18 15:32:24 +0000"),
+			author: "a second author"
+		)
+		let expectedRelativeDate1 = "1 day ago"
+		
+		let comment2 = makeComment(
+			message: "another message",
+			creationDate: makeDateFromTimestamp(1_604_571_429, description: "2020-11-05 10:17:09 +0000"),
+			author: "a third author"
+		)
+		let expectedRelativeDate2 = "2 weeks ago"
+		
+		let comment3 = makeComment(
+			message: "a fourth message",
+			creationDate: makeDateFromTimestamp(1_602_510_149, description: "2020-10-12 13:42:29 +0000"),
+			author: "another author"
+		)
+		let expectedRelativeDate3 = "1 month ago"
+		
+		let comment4 = makeComment(
+			message: "a fifth message",
+			creationDate: makeDateFromTimestamp(1_488_240_000, description: "2017-02-28 00:00:00 +0000"),
+			author: "a fifth author"
+		)
+		let expectedRelativeDate4 = "3 years ago"
+		
+		
+		let (sut, loader) = makeSUT(url: anyURL(), currentDate: { staticDate })
+		
+		sut.loadViewIfNeeded()
+		assertThat(sut, isRendering: [])
+		
+		loader.completeCommentsLoading(with: [comment0], at: 0)
+		assertThat(sut, isRendering: [(comment0, expectedRelativeDate0)])
+		
+		sut.simulateUserInitiatedReloading()
+		loader.completeCommentsLoading(with: [comment0, comment1, comment2, comment3, comment4], at: 1)
+		assertThat(sut, isRendering: [
+			(comment0, expectedRelativeDate0),
+			(comment1, expectedRelativeDate1),
+			(comment2, expectedRelativeDate2),
+			(comment3, expectedRelativeDate3),
+			(comment4, expectedRelativeDate4)
+		])
+	}
+	
 	// MARK: - Helpers
 	
-	private func makeSUT(url: URL, file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageCommentsViewController, loader: LoaderSpy) {
+	private func makeSUT(url: URL, currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageCommentsViewController, loader: LoaderSpy) {
 		let loader = LoaderSpy()
-		let sut = ImageCommentsViewController(url: url, loader: loader)
+		let sut = ImageCommentsViewController(url: url, currentDate: currentDate, loader: loader)
 		trackForMemoryLeaks(loader, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, loader)
+	}
+	
+	private func assertThat(_ sut: ImageCommentsViewController, isRendering pairs: [(comment: ImageComment, relativeDate: String)], file: StaticString = #filePath, line: UInt = #line) {
+		guard sut.numberOfRenderedComments() == pairs.count else {
+			return XCTFail("Expected \(pairs.count) images, got \(sut.numberOfRenderedComments()) instead.", file: file, line: line)
+		}
+		
+		pairs.enumerated().forEach { index, pair in
+			assertThat(sut, hasViewConfiguredFor: pair, at: index, file: file, line: line)
+		}
+	}
+	
+	private func assertThat(_ sut: ImageCommentsViewController, hasViewConfiguredFor pair: (comment: ImageComment, relativeDate: String), at index: Int, file: StaticString = #filePath, line: UInt = #line) {
+		let view = sut.imageCommentView(at: index)
+		
+		guard let cell = view as? ImageCommentCell else {
+			return XCTFail("Expected \(ImageCommentCell.self) instance, got \(String(describing: self)) instead", file: file, line: line)
+		}
+		
+		XCTAssertEqual(cell.authorText, pair.comment.author, "Expected author to be \(pair.comment.author) for comment view at index \(index)", file: file, line: line)
+		
+		XCTAssertEqual(cell.messageText, pair.comment.message, "Expected message to be \(pair.comment.message) for comment view at index \(index)", file: file, line: line)
+		
+		XCTAssertEqual(cell.creationDateText, pair.relativeDate, "Expected relative date to be \(pair.relativeDate) for comment view at index \(index)", file: file, line: line)
+	}
+	
+	private func makeDateFromTimestamp(_ timestamp: TimeInterval, description: String, file: StaticString = #file, line: UInt = #line) -> Date {
+		let date = Date(timeIntervalSince1970: timestamp)
+		XCTAssertEqual(date.description, description, file: file, line: line)
+		return date
+	}
+	
+	private func anyURL() -> URL {
+		URL(string: "https://any-url.com")!
+	}
+	
+	private func makeComment(message: String, creationDate: Date, author: String) -> ImageComment {
+		ImageComment(id: UUID(), message: message, creationDate: creationDate, author: author)
 	}
 	
 	class LoaderSpy: ImageCommentLoader {
@@ -74,19 +169,47 @@ class ImageCommentsViewControllerTests: XCTestCase {
 			return Task()
 		}
 		
-		func completeCommentsLoading(at index: Int = 0) {
-			completions[index](.success([]))
+		func completeCommentsLoading(with comments: [ImageComment] = [], at index: Int = 0) {
+			completions[index](.success(comments))
 		}
 	}
 }
 
 extension ImageCommentsViewController {
+	private var commentsSection: Int {
+		0
+	}
+	
 	var isShowingLoadingSpinner: Bool {
 		refreshControl?.isRefreshing == true
 	}
 	
 	func simulateUserInitiatedReloading() {
 		refreshControl?.simulatePullToRefresh()
+	}
+	
+	func numberOfRenderedComments() -> Int {
+		tableView.numberOfRows(inSection: commentsSection)
+	}
+	
+	func imageCommentView(at row: Int) -> UITableViewCell? {
+		let dataSource = tableView.dataSource
+		let indexPath = IndexPath(row: row, section: commentsSection)
+		return dataSource?.tableView(tableView, cellForRowAt: indexPath)
+	}
+}
+
+extension ImageCommentCell {
+	var authorText: String? {
+		authorLabel.text
+	}
+	
+	var creationDateText: String? {
+		creationDateLabel.text
+	}
+	
+	var messageText: String? {
+		messageLabel.text
 	}
 }
 
