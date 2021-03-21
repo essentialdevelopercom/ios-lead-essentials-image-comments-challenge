7c525b6ba5ee7e61a63b3d7d7cc4e506e066bdb3
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-03-21 18:15:30 +0500

    Add FeedCommentsPresenter tests

---
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedCommentsPresenterTests.swift
@@ -0,0 +1,19 @@
+//
+//  Created by Azamat Valitov on 21.03.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedCommentsPresenter {
+	
+}
+
+class FeedCommentsPresenterTests: XCTestCase {
+	
+	func test() {
+		let sut = FeedCommentsPresenter()
+	}
+	
+}
