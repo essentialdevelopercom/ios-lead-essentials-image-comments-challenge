d04ee4b0b76718c384ff5f138ad2728ae5438a9d
Ángel Vázquez <angel_betta@outlook.com>
2021-03-20 18:42:25 -0600

    Load comments on view did load

--- EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
@@ -8,10 +8,20 @@
 
 import EssentialFeed
 import XCTest
+import UIKit
 
-class ImageCommentsViewController {
-	init(loader: ImageCommentsViewControllerTests.LoaderSpy) {
-		
+class ImageCommentsViewController: UIViewController {
+	
+	private var loader: ImageCommentsViewControllerTests.LoaderSpy?
+	
+	convenience init(loader: ImageCommentsViewControllerTests.LoaderSpy) {
+		self.init()
+		self.loader = loader
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		loader?.load()
 	}
 }
 
@@ -23,9 +33,22 @@ class ImageCommentsViewControllerTests: XCTestCase {
 		XCTAssertEqual(loader.loadCallCount, 0)
 	}
 	
+	func test_viewDidLoad_loadsComments() {
+		let loader = LoaderSpy()
+		let sut = ImageCommentsViewController(loader: loader)
+		
+		sut.loadViewIfNeeded()
+		
+		XCTAssertEqual(loader.loadCallCount, 1)
+	}
+	
 	// MARK: - Helpers
 	
 	class LoaderSpy {
 		private(set) var loadCallCount = 0
+		
+		func load() {
+			loadCallCount += 1
+		}
 	}
 }
