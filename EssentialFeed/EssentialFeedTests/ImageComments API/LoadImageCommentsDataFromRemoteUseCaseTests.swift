7e564fcde11933f50221a42bbd5df5c27c83944e
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-03-22 19:19:24 +0200

    test load requests data from URL

--- EssentialFeed/EssentialFeedTests/ImageComments API/LoadImageCommentsDataFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageComments API/LoadImageCommentsDataFromRemoteUseCaseTests.swift
@@ -7,18 +7,40 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class RemoteImageCommentsLoader {
+	private let url: URL
+	private let client: HTTPClient
 
+	init(url: URL, client: HTTPClient) {
+		self.url = url
+		self.client = client
+	}
+
+	func load() {
+		client.get(from: url) { _ in }
+	}
 }
 
 class LoadImageCommentsDataFromRemoteUseCaseTests: XCTestCase {
 
 	func test_init_doesNotRequestDataFromURL() {
+		let url = URL(string: "https://a-given-url.com")!
 		let client = HTTPClientSpy()
-		let _ = RemoteImageCommentsLoader()
+		let _ = RemoteImageCommentsLoader(url: url, client: client)
 
 		XCTAssertTrue(client.requestedURLs.isEmpty)
+	}
+
+	func test_load_requestsDataFromURL() {
+		let url = URL(string: "https://a-given-url.com")!
+		let client = HTTPClientSpy()
+		let sut = RemoteImageCommentsLoader(url: url, client: client)
+
+		sut.load()
+
+		XCTAssertEqual(client.requestedURLs, [url])
 	}
 
 }
