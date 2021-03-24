240850ba8ac451560d60dc8331c677f6e9f1788e
Ángel Vázquez <angel_betta@outlook.com>
2021-03-23 21:16:38 -0600

    Move `ImageCommentsListPresenter` and its collaborators to its own file

--- EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsRefreshController.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsRefreshController.swift
@@ -9,57 +9,6 @@
 import EssentialFeed
 import UIKit
 
-struct ImageCommentLoadingViewModel {
-	let isLoading: Bool
-}
-
-protocol ImageCommentLoadingView {
-	func display(_ viewModel: ImageCommentLoadingViewModel)
-}
-
-struct ImageCommentsListViewModel {
-	let comments: [ImageComment]
-}
-
-protocol ImageCommentsListView {
-	func display(_ viewModel: ImageCommentsListViewModel)
-}
-
-struct ImageCommentErrorViewModel {
-	let message: String?
-}
-
-protocol ImageCommentErrorView {
-	func display(_ viewModel: ImageCommentErrorViewModel)
-}
-
-final class ImageCommentsListPresenter {
-	private let loadingView: ImageCommentLoadingView
-	private let commentsView: ImageCommentsListView
-	private let errorView: ImageCommentErrorView
-	
-	init(loadingView: ImageCommentLoadingView, commentsView: ImageCommentsListView, errorView: ImageCommentErrorView) {
-		self.loadingView = loadingView
-		self.commentsView = commentsView
-		self.errorView = errorView
-	}
-	
-	func didStartLoadingComments() {
-		loadingView.display(ImageCommentLoadingViewModel(isLoading: true))
-		errorView.display(ImageCommentErrorViewModel(message: nil))
-	}
-	
-	func didFinishLoadingComments(with comments: [ImageComment]) {
-		loadingView.display(ImageCommentLoadingViewModel(isLoading: false))
-		commentsView.display(ImageCommentsListViewModel(comments: comments))
-	}
-	
-	func didFinishLoadingComments(with error: Error) {
-		loadingView.display(ImageCommentLoadingViewModel(isLoading: false))
-		errorView.display(ImageCommentErrorViewModel(message: "Couldn't connect to server"))
-	}
-}
-
 protocol ImageCommentsRefreshViewControllerDelegate {
 	func didRequestLoadingComments()
 }
