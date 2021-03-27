38c16f01cf35b79c2fd61c9c57a136d54e0d231d
Bogdan Poplauschi <bpoplauschi@gmail.com>
2021-03-27 17:04:44 +0200

    `RemoteCommentsLoader` does not request data on init

---
+++ EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentsFromRemoteUseCaseTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoadImageCommentsFromRemoteUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Bogdan Poplauschi on 27/03/2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+import XCTest
+
+final class RemoteCommentsLoader {
+	init(url: URL, client _: HTTPClient) {}
+}
+
+final class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
+	func test_init_doesNotRequestDataFromURL() {
+		let (_, client) = makeSUT()
+		
+		XCTAssertTrue(client.requestedURLs.isEmpty)
+	}
+	
+	// MARK: - Helpers
+	
+	private func makeSUT(url: URL = anyURL(), file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteCommentsLoader, client: HTTPClientSpy) {
+		let client = HTTPClientSpy()
+		let sut = RemoteCommentsLoader(url: url, client: client)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		trackForMemoryLeaks(client, file: file, line: line)
+		return (sut, client)
+	}
+}
