55a890d45ca756e8f710e928d8b9c3ebca2d407d
Ángel Vázquez <angel_betta@outlook.com>
2021-03-26 22:41:42 -0600

    Move system under test (SUT) creation to a test factory method

--- EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsPresenterTests.swift
@@ -16,14 +16,20 @@ final class ImageCommentsListPresenter {
 
 class ImageCommentsListPresenterTests: XCTestCase {
 	func test_init_doesNotSendMessagesToView() {
-		let view = ViewSpy()
-		
-		_ = ImageCommentsListPresenter(view: view)
+		let (_, view) = makeSUT()
 		
 		XCTAssertTrue(view.messages.isEmpty)
 	}
 	
 	// MARK: - Helpers
+	
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageCommentsListPresenter, view: ViewSpy) {
+		let view = ViewSpy()
+		let sut = ImageCommentsListPresenter(view: view)
+		trackForMemoryLeaks(view, file: file, line: line)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return (sut, view)
+	}
 	
 	private class ViewSpy {
 		let messages = [Any]()
