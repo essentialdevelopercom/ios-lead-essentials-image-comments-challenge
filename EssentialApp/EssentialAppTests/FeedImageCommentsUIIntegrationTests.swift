8318c15d14e3cafbfb2abe64ab4f5cf866f6c3dd
Ivan <ivan.ornes@gmail.com>
2021-03-27 22:02:26 +0100

    Testing that the `FeedImageCommentsUIComposer` composes a view controller with the localized `FEED_COMMENTS_VIEW_TITLE` title

---
+++ EssentialApp/EssentialAppTests/FeedImageCommentsUIIntegrationTests.swift
@@ -0,0 +1,40 @@
+//
+//  FeedImageCommentsUIIntegrationTests.swift
+//  EssentialAppTests
+//
+//  Created by Ivan Ornes on 27/3/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import UIKit
+import EssentialApp
+import EssentialFeed
+import EssentialFeediOS
+
+final class FeedImageCommentsUIIntegrationTests: XCTestCase {
+	
+	func test_feedImageCommentsView_hasTitle() {
+		let (sut, _) = makeSUT()
+		
+		sut.loadViewIfNeeded()
+		
+		XCTAssertEqual(sut.title, localized("FEED_COMMENTS_VIEW_TITLE"))
+	}
+	
+	// MARK: - Helpers
+	
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedImageCommentsViewController, loader: LoaderSpy) {
+		let loader = LoaderSpy()
+		
+		let sut = FeedImageCommentsUIComposer.feedImageCommentsComposedWith(feedImage: makeImage(),
+																			feedImageCommentsLoader: loader.loadPublisher)
+		trackForMemoryLeaks(loader, file: file, line: line)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return (sut, loader)
+	}
+	
+	private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "http://any-url.com")!) -> FeedImage {
+		return FeedImage(id: UUID(), description: description, location: location, url: url)
+	}
+}
