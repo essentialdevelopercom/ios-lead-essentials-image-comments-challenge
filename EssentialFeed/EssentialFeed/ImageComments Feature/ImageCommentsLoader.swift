0ff18ef711f0a657913b3f34a8b2b32605b33c21
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-03-27 21:38:50 +0200

    remove Foundation import from `ImageCommentsLoader`

--- EssentialFeed/EssentialFeed/ImageComments Feature/ImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/ImageComments Feature/ImageCommentsLoader.swift
@@ -6,8 +6,6 @@
 //  Copyright © 2021 Essential Developer. All rights reserved.
 //
 
-import Foundation
-
 public protocol ImageCommentsLoader {
 	typealias Result = Swift.Result<[ImageComment], Error>
 
