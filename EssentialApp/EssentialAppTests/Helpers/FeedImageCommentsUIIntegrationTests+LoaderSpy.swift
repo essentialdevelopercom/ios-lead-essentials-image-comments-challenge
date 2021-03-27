c0cadbe5166753b86130e6325ed66ba7529244e0
Ivan <ivan.ornes@gmail.com>
2021-03-27 22:06:09 +0100

    `FeedImageCommentsUIIntegrationTests` extension with custom `LoaderSpy`

---
+++ EssentialApp/EssentialAppTests/Helpers/FeedImageCommentsUIIntegrationTests+LoaderSpy.swift
@@ -0,0 +1,36 @@
+//
+//  FeedImageCommentsUIIntegrationTests+LoaderSpy.swift
+//  EssentialAppTests
+//
+//  Created by Ivan Ornes on 27/3/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+import EssentialFeediOS
+
+extension FeedImageCommentsUIIntegrationTests {
+	
+	class LoaderSpy: FeedImageCommentsLoader {
+		
+		// MARK: - FeedImageCommentsLoader
+		
+		private struct ImageCommentTaskSpy: FeedImageCommentsLoaderTask {
+			let cancelCallback: () -> Void
+			func cancel() {
+				cancelCallback()
+			}
+		}
+		
+		private var imageCommentRequests = [(imageID: String, completion: (FeedImageCommentsLoader.Result) -> Void)]()
+		
+		private(set) var cancelledImageIDs = [String]()
+		
+		func loadImageComments(imageID: String, completion: @escaping (FeedImageCommentsLoader.Result) -> Void) -> FeedImageCommentsLoaderTask {
+			imageCommentRequests.append((imageID, completion))
+			return ImageCommentTaskSpy { [weak self] in self?.cancelledImageIDs.append(imageID) }
+		}
+	}
+	
+}
