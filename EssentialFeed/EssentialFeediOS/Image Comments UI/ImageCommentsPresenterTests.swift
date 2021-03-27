ebfdf965e96e4ca0692f80b53a9a3e3b1dff1faf
Ángel Vázquez <angel_betta@outlook.com>
2021-03-26 22:51:49 -0600

    `ImageCommentsListPresenter` displays no error on `didStartLoadingComments`

--- EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/ImageCommentsPresenterTests.swift
@@ -8,9 +8,24 @@
 
 import XCTest
 
+struct ImageCommentsErrorViewModel {
+	let message: String?
+}
+
+protocol ImageCommentsErrorView {
+	func display(_ viewModel: ImageCommentsErrorViewModel)
+}
+
 final class ImageCommentsListPresenter {
-	init(view: Any) {
-		
+	
+	private let errorView: ImageCommentsErrorView
+	
+	init(errorView: ImageCommentsErrorView) {
+		self.errorView = errorView
+	}
+	
+	func didStartLoadingComments() {
+		errorView.display(ImageCommentsErrorViewModel(message: nil))
 	}
 }
 
@@ -21,18 +36,35 @@ class ImageCommentsListPresenterTests: XCTestCase {
 		XCTAssertTrue(view.messages.isEmpty)
 	}
 	
+	func test_didStartLoadingComments_displaysNoErrorMessage() {
+		let (sut, view) = makeSUT()
+		
+		sut.didStartLoadingComments()
+		
+		XCTAssertEqual(view.messages, [.display(errorMessage: .none)])
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageCommentsListPresenter, view: ViewSpy) {
 		let view = ViewSpy()
-		let sut = ImageCommentsListPresenter(view: view)
+		let sut = ImageCommentsListPresenter(errorView: view)
 		trackForMemoryLeaks(view, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		return (sut, view)
 	}
 	
-	private class ViewSpy {
-		let messages = [Any]()
+	private class ViewSpy: ImageCommentsErrorView {
+		
+		enum Messages: Equatable {
+			case display(errorMessage: String?)
+		}
+		
+		private(set) var messages = [Messages]()
+		
+		func display(_ viewModel: ImageCommentsErrorViewModel) {
+			messages.append(.display(errorMessage: viewModel.message))
+		}
 	}
 }
 
