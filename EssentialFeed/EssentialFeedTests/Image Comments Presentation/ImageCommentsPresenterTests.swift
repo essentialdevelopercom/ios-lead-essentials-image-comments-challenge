0a37365dc37116943abb71f0e7b7994a7098b3db
Bogdan Poplauschi <bpoplauschi@gmail.com>
2021-03-28 14:11:47 +0300

    `ImageCommentsPresenter` displays no error message and starts loading on start loading comments

--- EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments Presentation/ImageCommentsPresenterTests.swift
@@ -8,13 +8,34 @@
 
 import XCTest
 
+protocol ImageCommentsLoadingView {
+	func display(isLoading: Bool)
+}
+
+protocol ImageCommentsErrorView {
+	func display(errorMessage: String?)
+}
+
 final class ImageCommentsPresenter {
+	private let loadingView: ImageCommentsLoadingView
+	private let errorView: ImageCommentsErrorView
+	
 	public static var title: String {
 		NSLocalizedString(
 			"IMAGE_COMMENTS_VIEW_TITLE",
 			tableName: "ImageComments",
 			bundle: Bundle(for: ImageCommentsPresenter.self),
 			comment: "Title for the image comments view")
+	}
+	
+	public init(loadingView: ImageCommentsLoadingView, errorView: ImageCommentsErrorView) {
+		self.loadingView = loadingView
+		self.errorView = errorView
+	}
+	
+	public func didStartLoadingComments() {
+		loadingView.display(isLoading: true)
+		errorView.display(errorMessage: nil)
 	}
 }
 
@@ -23,7 +44,30 @@ final class ImageCommentsPresenterTests: XCTestCase {
 		XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
 	}
 	
+	func test_init_doesNotSendMessagesToView() {
+		let (_, view) = makeSUT()
+		
+		XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+	}
+	
+	func test_didStartLoadingComments_displaysNoErrorMessagesAndStartsLoading() {
+		let view = ViewSpy()
+		let sut = ImageCommentsPresenter(loadingView: view, errorView: view)
+		
+		sut.didStartLoadingComments()
+		
+		XCTAssertEqual(view.messages, [.display(isLoading: true), .display(errorMessage: nil)])
+	}
+	
 	// MARK: - Helpers
+	
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ImageCommentsPresenter, view: ViewSpy) {
+		let view = ViewSpy()
+		let sut = ImageCommentsPresenter(loadingView: view, errorView: view)
+		trackForMemoryLeaks(view, file: file, line: line)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return (sut, view)
+	}
 	
 	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
 		let table = "ImageComments"
@@ -33,5 +77,22 @@ final class ImageCommentsPresenterTests: XCTestCase {
 			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
 		}
 		return value
+	}
+	
+	private class ViewSpy: ImageCommentsLoadingView, ImageCommentsErrorView {
+		enum Message: Equatable {
+			case display(isLoading: Bool)
+			case display(errorMessage: String?)
+		}
+		
+		private(set) var messages = [Message]()
+		
+		func display(isLoading: Bool) {
+			messages.append(.display(isLoading: isLoading))
+		}
+		
+		func display(errorMessage: String?) {
+			messages.append(.display(errorMessage: errorMessage))
+		}
 	}
 }
