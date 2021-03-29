dd65d582e38faa9698b271e70d705a2e34e013a5
Ángel Vázquez <angel_betta@outlook.com>
2021-03-28 21:27:45 -0600

    Add animations for comment error view

--- EssentialFeed/EssentialFeediOS/Image Comments UI/CommentErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/CommentErrorView.swift
@@ -12,15 +12,34 @@ public class CommentErrorView: UIView {
 	
 	@IBOutlet public var button: UIButton?
 	
+	public override func awakeFromNib() {
+		super.awakeFromNib()
+		button?.setTitle(nil, for: .normal)
+		alpha = 0
+	}
+	
+	private var isVisible: Bool { alpha > 0 }
+	
 	public var message: String? {
-		get { return button?.title(for: .normal) }
+		get { return isVisible ? button?.title(for: .normal) : .none }
 	}
 	
 	func show(message: String) {
 		button?.setTitle(message, for: .normal)
+		
+		UIView.animate(withDuration: 0.25) {
+			self.alpha = 1
+		}
 	}
 	
 	@IBAction func hideMessage() {
-		button?.setTitle(nil, for: .normal)
+		UIView.animate(
+			withDuration: 0.25,
+			animations: { self.alpha = 0 },
+			completion: { completed in
+				if completed {
+					self.button?.setTitle(nil, for: .normal)
+				}
+			})
 	}
 }
