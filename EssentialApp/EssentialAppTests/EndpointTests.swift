f6f7fee38c784d15602c2dfb3fd6b3dc179e3c22
Ángel Vázquez <angel_betta@outlook.com>
2021-03-28 21:42:13 -0600

    `Endpoint` returns correct URL for image comment for a given image ID

--- EssentialApp/EssentialAppTests/EndpointTests.swift
+++ EssentialApp/EssentialAppTests/EndpointTests.swift
@@ -10,6 +10,7 @@ import XCTest
 
 enum Endpoint {
 	case feed
+	case imageComments(id: String)
 	
 	static var baseURL: URL {
 		URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/")!
@@ -19,6 +20,8 @@ enum Endpoint {
 		switch endpoint {
 		case .feed:
 			return baseURL.appendingPathComponent("v1/feed")
+		case let .imageComments(id: id):
+			return baseURL.appendingPathComponent("v1/image/\(id)/comments")
 		}
 	}
 }
@@ -28,5 +31,12 @@ class EndpointTests: XCTestCase {
 		let feedURL = Endpoint.url(for: .feed)
 		
 		XCTAssertEqual(feedURL.absoluteString, "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")
+	}
+	
+	func test_imageCommentsURL_returnsCorrectURL() {
+		let imageID = UUID().uuidString
+		let imageCommentsURL = Endpoint.url(for: .imageComments(id: imageID))
+		
+		XCTAssertEqual(imageCommentsURL.absoluteString, "https://ile-api.essentialdeveloper.com/essential-feed/v1/image/\(imageID)/comments")
 	}
 }
