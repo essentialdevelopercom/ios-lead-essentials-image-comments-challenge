6325eab7ffdd7ebd685614c19175cfea375089d1
Ángel Vázquez <angel_betta@outlook.com>
2021-03-28 19:59:15 -0600

    Move `ImageCommentsViewController` DSL test helpers to their own file

--- EssentialApp/EssentialAppTests/ImageCommentsIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/ImageCommentsIntegrationTests.swift
@@ -312,39 +312,3 @@ class ImageCommentsIntegrationTests: XCTestCase {
 		}
 	}
 }
-
-extension ImageCommentsViewController {
-	private var commentsSection: Int {
-		0
-	}
-	
-	private var errorView: CommentErrorView? {
-		tableView.tableHeaderView as? CommentErrorView
-	}
-	
-	var errorMessage: String? {
-		return errorView?.message
-	}
-	
-	var isShowingLoadingSpinner: Bool {
-		refreshControl?.isRefreshing == true
-	}
-	
-	func simulateUserInitiatedReloading() {
-		refreshControl?.simulatePullToRefresh()
-	}
-	
-	func simulateTapOnErrorMessage() {
-		errorView?.button?.simulateTap()
-	}
-	
-	func numberOfRenderedComments() -> Int {
-		tableView.numberOfRows(inSection: commentsSection)
-	}
-	
-	func imageCommentView(at row: Int) -> UITableViewCell? {
-		let dataSource = tableView.dataSource
-		let indexPath = IndexPath(row: row, section: commentsSection)
-		return dataSource?.tableView(tableView, cellForRowAt: indexPath)
-	}
-}
