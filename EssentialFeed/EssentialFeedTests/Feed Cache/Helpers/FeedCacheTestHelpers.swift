605a8a6f7b403eaa74b47c56797a8d24d5e9ae35
Danil <Danil.Vassyakin@kaspi.kz>
2021-03-31 13:39:07 +0600

    move remote comments tests to API folder

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -14,4 +14,3 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
 	let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
 	return (models, local)
 }
-
