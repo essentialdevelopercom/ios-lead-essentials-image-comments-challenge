fc9711452fe80a56ea0e9d48a4f2b3f3e35a4e2c
Ivan <ivan.ornes@gmail.com>
2021-04-01 23:19:59 +0200

    Removed weak property from the `FeedViewAdapter` display function, weak is not needed since the compactMap function is a synchronous function

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -20,8 +20,7 @@ final class FeedViewAdapter: FeedView {
 	}
 	
 	func display(_ viewModel: FeedViewModel) {
-		controller?.display(viewModel.feed.compactMap { [weak self] model in
-			guard let self = self else { return nil }
+		controller?.display(viewModel.feed.compactMap { model in
 			let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: self.imageLoader, imageSelection: self.imageSelection)
 			let view = FeedImageCellController(delegate: adapter)
 			
