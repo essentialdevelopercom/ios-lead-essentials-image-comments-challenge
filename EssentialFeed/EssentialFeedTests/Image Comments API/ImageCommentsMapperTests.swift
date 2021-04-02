fbc542f6bc949549793e56625190f0dacd827412
Dan Hung <dan12411@gmail.com>
2021-04-02 16:47:41 +0800

    Network Module: Test ImageCommentsMapper in isolation

---
+++ EssentialFeed/EssentialFeedTests/Image Comments API/ImageCommentsMapperTests.swift
@@ -0,0 +1,84 @@
+import XCTest
+import EssentialFeed
+
+class ImageCommentsMapperTests: XCTestCase {
+
+	func test_map_throwsErrorOnNon2xxHTTPResponse() throws {
+		let json = makeItemsJSON([])
+
+		let samples = [199, 101, 300, 400, 500]
+
+		try samples.forEach { code in
+			XCTAssertThrowsError(
+				try ImageCommentsMapper.map(json, from: HTTPURLResponse(code: code))
+			)
+		}
+	}
+
+	func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
+		let invalidJSON = Data("invalid json".utf8)
+		let samples = [200, 201, 233, 250, 299]
+
+		try samples.forEach { code in
+			XCTAssertThrowsError(
+				try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(code: code))
+			)
+		}
+	}
+
+	func test_map_deliversNoItemsOn2xxHTTPResponseWithEmptyJSONList() throws {
+		let emptyListJSON = makeItemsJSON([])
+		let samples = [200, 201, 233, 250, 299]
+
+		try samples.forEach { code in
+			let result = try ImageCommentsMapper.map(emptyListJSON, from: HTTPURLResponse(code: code))
+			XCTAssertEqual(result, [])
+		}
+	}
+
+	func test_map_deliversItemsOn2xxHTTPResponseWithJSONItems() throws {
+		let item1 = makeItem(
+			id: UUID(),
+			message: "a message",
+			createdAt: ISO8601DateFormatter().date(from: "2020-05-20T11:24:59+0000")!,
+			username: "a username")
+
+		let item2 = makeItem(
+			id: UUID(),
+			message: "another message",
+			createdAt: ISO8601DateFormatter().date(from: "2020-05-20T11:24:59+0000")!,
+			username: "another username")
+
+		let json = makeItemsJSON([item1.json, item2.json])
+
+		let samples = [200, 201, 233, 250, 299]
+
+		try samples.forEach { code in
+			let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(code: code))
+			XCTAssertEqual(result, [item1.model, item2.model])
+		}
+	}
+
+	// MARK: - Helpers
+
+	private func makeItem(id: UUID, message: String, createdAt: Date, username: String) -> (model: ImageComment, json: [String: Any]) {
+		let item = ImageComment(id: id, message: message, createdAt: createdAt, username: username)
+
+		let json = [
+			"id": id.uuidString,
+			"message": message,
+			"created_at": ISO8601DateFormatter().string(from: createdAt),
+			"author": [
+				"username" : username
+			]
+		].compactMapValues { $0 }
+
+		return (item, json)
+	}
+
+	private func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+		let json = ["items": items]
+		return try! JSONSerialization.data(withJSONObject: json)
+	}
+
+}
