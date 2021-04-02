36e19b56e6f6262a4e64da6440a58c8ddb954460
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-04-02 17:59:55 +0300

    show loading indicator in view did load

--- EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageCommentsViewControllerTests.swift
@@ -23,6 +23,7 @@ final class ImageCommentsViewController: UITableViewController {
 
 		refreshControl = UIRefreshControl()
 		refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+		refreshControl?.beginRefreshing()
 		load()
 	}
 
@@ -56,6 +57,14 @@ final class ImageCommentsViewControllerTests: XCTestCase {
 
 		sut.refreshControl?.simulatePullToRefresh()
 		XCTAssertEqual(loader.loadCallCount, 3)
+	}
+
+	func test_viewDidLoad_showsLoadingIndicator() {
+		let (sut, _) = makeSUT()
+
+		sut.loadViewIfNeeded()
+
+		XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
 	}
 
 	// MARK: - Helpers
