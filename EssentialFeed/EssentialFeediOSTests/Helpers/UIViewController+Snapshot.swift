89e77ad4e0d56d354efe9380f3b6d7098955bfeb
Azamat Valitov <valitov.azamat.m@gmail.com>
2021-04-03 23:45:26 +0500

    Improve snapshot tests helpers

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
@@ -16,7 +16,7 @@ struct SnapshotConfiguration {
 	let layoutMargins: UIEdgeInsets
 	let traitCollection: UITraitCollection
 	
-	static func iPhone8(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
+	static func iPhone8(style: UIUserInterfaceStyle, preferredContentSizeCategory: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
 		return SnapshotConfiguration(
 			size: CGSize(width: 375, height: 667),
 			safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
@@ -24,7 +24,7 @@ struct SnapshotConfiguration {
 			traitCollection: UITraitCollection(traitsFrom: [
 				.init(forceTouchCapability: .available),
 				.init(layoutDirection: .leftToRight),
-				.init(preferredContentSizeCategory: .medium),
+				.init(preferredContentSizeCategory: preferredContentSizeCategory),
 				.init(userInterfaceIdiom: .phone),
 				.init(horizontalSizeClass: .compact),
 				.init(verticalSizeClass: .regular),
@@ -34,23 +34,6 @@ struct SnapshotConfiguration {
 			]))
 	}
 	
-	static func iPhone8ExtraExtraLarge(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
-		return SnapshotConfiguration(
-			size: CGSize(width: 375, height: 667),
-			safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
-			layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
-			traitCollection: UITraitCollection(traitsFrom: [
-				.init(forceTouchCapability: .available),
-				.init(layoutDirection: .leftToRight),
-				.init(preferredContentSizeCategory: .extraExtraLarge),
-				.init(userInterfaceIdiom: .phone),
-				.init(horizontalSizeClass: .compact),
-				.init(verticalSizeClass: .regular),
-				.init(displayScale: 2),
-				.init(displayGamut: .P3),
-				.init(userInterfaceStyle: style)
-			]))
-	}
 }
 
 private final class SnapshotWindow: UIWindow {
