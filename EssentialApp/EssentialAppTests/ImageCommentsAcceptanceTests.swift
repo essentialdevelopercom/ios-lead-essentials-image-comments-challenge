07b7c23816438edcdf1a4fd2b523aa0d30bfe29f
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-04-04 14:28:23 +0300

    add `ImageCommentsAcceptanceTests`

---
+++ EssentialApp/EssentialAppTests/ImageCommentsAcceptanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCommentsAcceptanceTests.swift
+//  EssentialAppTests
+//
+//  Created by Sebastian Vidrea on 04.04.2021.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+final class ImageCommentsAcceptanceTests: XCTestCase {
+
+}
