1a9f6201885c6172d5f7bb1038fc2844c77190be
aamayorga <amayorga95@outlook.com>
2021-04-08 20:56:51 -0700

    Make sure all supported localizations have values

--- EssentialFeed/EssentialFeediOSTests/Image Comment UI/ImageCommentViewControllerTest.swift
+++ EssentialFeed/EssentialFeediOSTests/Image Comment UI/ImageCommentViewControllerTest.swift
@@ -192,6 +192,25 @@ class ImageCommentViewControllerTest: XCTestCase {
 		XCTAssertEqual(sut.errorViewLabel.text, localizedTitle)
 	}
 	
+	func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+		let table = "Feed"
+		let presentationBundle = Bundle(for: FeedPresenter.self)
+		let localizationBundles = allLocalizationBundles(in: presentationBundle)
+		let localizationStringKeys = allLocalizedStringKeys(in: localizationBundles, table: table)
+		
+		localizationBundles.forEach { (bundle, localization) in
+			localizationStringKeys.forEach { (key) in
+				let localizedString = bundle.localizedString(forKey: key, value: nil, table: table)
+				
+				if localizedString == key {
+					let languague = Locale.current.localizedString(forLanguageCode: localization) ?? ""
+					
+					XCTFail("Missing \(languague) \(localization) localized string for key: '\(key)' in table: '\(table)'")
+				}
+			}
+		}
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: ImageCommentViewController, loader: LoaderSpy) {
@@ -227,5 +246,36 @@ class ImageCommentViewControllerTest: XCTestCase {
 		let imageComment = makeImageComment(comment: "comment", date: relativeDate, authorName: "authorName")
 		loader.completeCommentLoading(with: [imageComment])
 		return sut.imageCommentView(at: 0) as! ImageCommentCell
+	}
+	
+	private typealias LocalizedBundle = (bundle: Bundle, localization: String)
+	
+	private func allLocalizationBundles(in bundle: Bundle, file: StaticString = #file, line: UInt = #line) -> [LocalizedBundle] {
+		return bundle.localizations.compactMap { localization in
+			guard
+				let path = bundle.path(forResource: localization, ofType: "lproj"),
+				let localizedBundle = Bundle(path: path)
+			else {
+				XCTFail("Couldn't find bundle for localization: \(localization)", file: file, line: line)
+				return nil
+			}
+			
+			return (localizedBundle, localization)
+		}
+	}
+	
+	private func allLocalizedStringKeys(in bundles: [LocalizedBundle], table: String, file: StaticString = #file, line: UInt = #line) -> Set<String> {
+		return bundles.reduce([]) { (acc, current) in
+			guard
+				let path = current.bundle.path(forResource: table, ofType: "strings"),
+				let strings = NSDictionary(contentsOfFile: path),
+				let keys = strings.allKeys as? [String]
+			else {
+				XCTFail("Couldn't load localized strings for localization: \(current.localization)", file: file, line: line)
+				return acc
+			}
+
+			return acc.union(Set(keys))
+		}
 	}
 }
