b53f16427a02698e80f84eb6ad40244f682304af
Anthony Ilinykh <anthonyilinykh@gmail.com>
2021-04-13 09:09:41 +0300

    Update EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
    Co-authored-by: Caio Zullo <caiozullo@gmail.com>

--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -238,8 +238,10 @@ private extension CommentsController {
 	}
 	
 	var numberOfRenderedCommentViews: Int {
-		return tableView.numberOfRows(inSection: 0)
+		return tableView.numberOfRows(inSection: commentsSection)
 	}
+	
+	var commentsSection: Int { 0 }
 	
 	var errorMessage: String? {
 		return errorView?.message
