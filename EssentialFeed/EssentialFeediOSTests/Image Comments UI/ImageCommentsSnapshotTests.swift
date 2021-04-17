2d72478f3131e832b4c5b8eb4609800e7e052a63
Dan Hung <dan12411@gmail.com>
2021-04-17 18:58:49 +0800

    UI Module: Implementation Image Comment UI

---
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,50 @@
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+
+	func test_listWithComments() {
+		let sut = makeSUT()
+
+		sut.display(comments())
+
+		record(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENT_light")
+		record(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENT_dark")
+	}
+
+	// MARK: - Helpers
+
+	private func makeSUT() -> ListViewController {
+		let bundle = Bundle(for: ListViewController.self)
+		let storyboard = UIStoryboard(name: "ImageComment", bundle: bundle)
+		let controller = storyboard.instantiateInitialViewController() as! ListViewController
+		controller.loadViewIfNeeded()
+		controller.tableView.showsVerticalScrollIndicator = false
+		controller.tableView.showsHorizontalScrollIndicator = false
+		return controller
+	}
+
+	private func comments() -> [CellController] {
+		return [
+			ImageCommentCellController(
+				model: ImageCommentViewModel(
+					message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+					date: "1000 years age",
+					username: "a long long long long username")
+			),
+			ImageCommentCellController(
+				model: ImageCommentViewModel(
+					message: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+					date: "5 days age",
+					username: "a long long username")
+			),
+			ImageCommentCellController(
+				model: ImageCommentViewModel(
+					message: "good",
+					date: "3 minutes age",
+					username: "a.")
+			)
+		]
+	}
+}
