c53fc8e815f37c637305280e66000172b63b79ca
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-04-19 20:15:36 +0300

    add ImageComments Endpoint tests

---
+++ EssentialFeed/EssentialFeedTests/ImageComments/API/ImageCommentsEndpointTests.swift
@@ -0,0 +1,18 @@
+//
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsEndpointTests: XCTestCase {
+	func test_imageComments_endpointURL() {
+		let baseURL = URL(string: "http://base-url.com")!
+		let imageID = UUID().uuidString
+
+		let received = ImageCommentsEndpoint.get.url(baseURL: baseURL, imageID: imageID)
+		let expected = URL(string: "http://base-url.com/v1/image/\(imageID)/comments")
+
+		XCTAssertEqual(received, expected)
+	}
+}
