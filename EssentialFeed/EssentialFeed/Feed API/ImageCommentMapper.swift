2761b8e7a859be089e9636ff8d0aaac73dc03039
aamayorga <amayorga95@outlook.com>
2021-04-21 08:00:49 -0700

    Delivers proper results on 2xx response

---
+++ EssentialFeed/EssentialFeed/Feed API/ImageCommentMapper.swift
@@ -0,0 +1,27 @@
+//
+//  ImageCommentMapper.swift
+//  EssentialFeed
+//
+//  Created by Antonio Mayorga on 4/21/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+	private struct Root: Decodable {
+		let items: [RemoteFeedItem]
+	}
+	
+	static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+		guard isOK(response), let root = try? JSONDecoder().decode(Root.self, from: data) else {
+			throw RemoteImageCommentsLoader.Error.invalidData
+		}
+
+		return root.items
+	}
+	
+	private static func isOK(_ response: HTTPURLResponse) -> Bool {
+		(200...299).contains(response.statusCode)
+	}
+}
