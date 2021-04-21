bab00eee67a6279c2b657eebcdb3a3d25547cd99
Sebastian Vidrea <vidreasebastian@gmail.com>
2021-04-21 17:50:40 +0300

    remove force unwrap and return optional String instead to prevent an app crash during tests

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -103,9 +103,9 @@ extension ListViewController {
 		cell(row: row, section: imageCommentsSection)
 	}
 
-	func commentMessage(at row: Int) -> String {
-		let imageCommentCell = imageCommentView(at: row) as! ImageCommentCell
-		return imageCommentCell.messageText!
+	func commentMessage(at row: Int) -> String? {
+		let cell = imageCommentView(at: row) as? ImageCommentCell
+		return cell?.messageText
 	}
 
 	private var imageCommentsSection: Int { 0 }
