564e4bfe28bc9da2290627fa912cd6d2ebd7bb9d
Antonio Mayorga <amayorga95@outlook.com>
2021-04-24 14:10:59 -0700

    Make RemoteLoader conform to FeedLoader inn Composition Root

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -20,25 +20,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 				.defaultDirectoryURL()
 				.appendingPathComponent("feed-store.sqlite"))
 	}()
-	
-	private lazy var remoteFeedLoader: RemoteFeedLoader = {
-		RemoteFeedLoader(
-			url: URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!,
-			client: httpClient)
-	}()
-	
+
 	private lazy var localFeedLoader: LocalFeedLoader = {
 		LocalFeedLoader(store: store, currentDate: Date.init)
 	}()
-	
-	private lazy var remoteImageLoader: RemoteFeedImageDataLoader = {
-		RemoteFeedImageDataLoader(client: httpClient)
-	}()
-	
-	private lazy var localImageLoader: LocalFeedImageDataLoader = {
-		LocalFeedImageDataLoader(store: store)
-	}()
-	
+
 	convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
 		self.init()
 		self.httpClient = httpClient
@@ -66,6 +52,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 	
 	private func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
+		let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+		
+		let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+		
 		return remoteFeedLoader
 			.loadPublisher()
 			.caching(to: localFeedLoader)
@@ -73,12 +63,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	}
 	
 	private func makeLocalImageLoaderWithRemoteFallback(url: URL) -> FeedImageDataLoader.Publisher {
+		let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
+		let localImageLoader = LocalFeedImageDataLoader(store: store)
+
 		return localImageLoader
 			.loadImageDataPublisher(from: url)
-			.fallback(to: { [remoteImageLoader, localImageLoader] in
+			.fallback(to: {
 				remoteImageLoader
 					.loadImageDataPublisher(from: url)
 					.caching(to: localImageLoader, using: url)
 			})
 	}
 }
+
+extension RemoteLoader: FeedLoader where Resource == [FeedImage] {}
