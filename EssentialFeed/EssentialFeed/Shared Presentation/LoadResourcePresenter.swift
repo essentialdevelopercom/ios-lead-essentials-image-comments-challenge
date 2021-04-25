6ee744d745d8ff8a3aba7b17c5af7cb7cbe2eae7
Antonio Mayorga <amayorga95@outlook.com>
2021-04-24 18:58:22 -0700

    Make LoadResourcePresenter generic over the Resource types and re-add LoadResourcePresenter

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,53 @@
+//
+//  LoadResourcePresenter.swift
+//  EssentialFeed
+//
+//  Created by Antonio Mayorga on 4/24/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public protocol ResourceView {
+	associatedtype ResourceViewModel
+	
+	func display(_ viewModel: ResourceViewModel)
+}
+
+public final class LoadResourcePresenter<Resource, View: ResourceView> {
+	public typealias Mapper = (Resource) -> View.ResourceViewModel
+	
+	private let resourceView: View
+	private let loadingView: FeedLoadingView
+	private let errorView: FeedErrorView
+	private let mapper: Mapper
+	
+	private var feedLoadError: String {
+		return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+				tableName: "Feed",
+				bundle: Bundle(for: FeedPresenter.self),
+				comment: "Error message displayed when we can't load the image feed from the server")
+	}
+	
+	public init(resourceView: View, loadingView: FeedLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper) {
+		self.resourceView = resourceView
+		self.loadingView = loadingView
+		self.errorView = errorView
+		self.mapper = mapper
+	}
+		
+	public func didStartLoading() {
+		errorView.display(.noError)
+		loadingView.display(FeedLoadingViewModel(isLoading: true))
+	}
+	
+	public func didFinishLoading(with resource: Resource) {
+		resourceView.display(mapper(resource))
+		loadingView.display(FeedLoadingViewModel(isLoading: false))
+	}
+	
+	public func didFinishLoadingFeed(with error: Error) {
+		errorView.display(.error(message: feedLoadError))
+		loadingView.display(FeedLoadingViewModel(isLoading: false))
+	}
+}
