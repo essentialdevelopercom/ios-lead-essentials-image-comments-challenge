43f42bc71ea0b652e3152b72951024c665b2ec84
Dan Hung <dan12411@gmail.com>
2021-05-08 15:39:13 +0800

    Main Module: Fix storyboard naming

--- EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -18,7 +18,7 @@ class ImageCommentsSnapshotTests: XCTestCase {
 
 	private func makeSUT() -> ListViewController {
 		let bundle = Bundle(for: ListViewController.self)
-		let storyboard = UIStoryboard(name: "ImageComment", bundle: bundle)
+		let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
 		let controller = storyboard.instantiateInitialViewController() as! ListViewController
 		controller.loadViewIfNeeded()
 		controller.tableView.showsVerticalScrollIndicator = false
