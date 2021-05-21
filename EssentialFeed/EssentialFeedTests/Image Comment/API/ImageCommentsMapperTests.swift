38fee0590f1e54a5359aa7631e761b46fcc03edc
Nadheer Chatharoo <>
2021-05-21 09:17:26 +0200

    Throw error on 2XX HTTPResponse with invalid JSON

--- EssentialFeed/EssentialFeedTests/Image Comment/API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comment/API/ImageCommentsMapperTests.swift
@@ -17,13 +17,17 @@ class ImageCommentsMapperTests: XCTestCase {
 		}
 	}
 
-//	func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
-//		let invalidJSON = Data("invalid json".utf8)
-//
-//		XCTAssertThrowsError(
-//			try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200))
-//		)
-//	}
+	func test_map_throwsErrorOn2XXHTTPResponseWithInvalidJSON() throws {
+		let invalidJSON = Data("invalid json".utf8)
+		let samples = [200, 201, 250, 270, 299]
+
+		try samples.forEach { code in
+			XCTAssertThrowsError(
+				try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+			)
+		}
+	}
+
 //
 //	func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
 //		let emptyListJSON = makeItemsJSON([])
