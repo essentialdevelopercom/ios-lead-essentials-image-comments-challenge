47c5783eae2e37ea78e239998f621304c85b5beb
Nadheer Chatharoo <>
2021-05-26 15:30:54 +0200

    Record snapshots using 'iPhone 12 simulator' running iOS 14.4

--- EssentialFeed/EssentialFeediOSTests/Image Comment UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Image Comment UI/ImageCommentsSnapshotTests.swift
@@ -15,6 +15,9 @@ class ImageCommentsSnapshotTests: XCTestCase {
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_WITH_CONTENT_light")
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_WITH_CONTENT_dark")
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)), named: "IMAGE_COMMENTS_WITH_CONTENT_light_extraExtraExtraLarge")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENTS_WITH_CONTENT_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENTS_WITH_CONTENT_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)), named: "IMAGE_COMMENTS_WITH_CONTENT_light_extraExtraExtraLarge")
 	}
 
 	// MARK: - Helpers
