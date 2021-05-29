a79d858f7b26c278597cd74112e4c72dc1fbe69a
Ilhansari <ilhansari94@hotmail.com>
2021-05-29 15:08:55 +0300

    Add `ÌmageCommentsEndPoint` logic

--- EssentialFeed/EssentialFeed/Image Comments/API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/Image Comments/API/ImageCommentsMapper.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-final class ImageCommentsMapper {
+public final class ImageCommentsMapper {
 	private struct Root: Decodable {
 		private let items: [Item]
 
@@ -27,7 +27,7 @@ final class ImageCommentsMapper {
 		}
 	}
 
-	static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+	static public func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
 		let decoder = JSONDecoder()
 		decoder.dateDecodingStrategy = .iso8601
 
