baaf603a50bb2fbda0211b1274b44fbc7931c206
Vita Solomina <vitaliaa@gmail.com>
2021-05-30 20:04:58 -0700

    Use ImageComment in ImageCommentsMapper

--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
@@ -6,9 +6,9 @@ import XCTest
 import EssentialFeed
 
 class ImageCommentsMapperTests: XCTestCase {
-	func test_map_throwsErrorOnNon200HTTPResponse() throws {
+	func test_map_throwsErrorOnNon2xxHTTPResponse() throws {
 		let json = makeItemsJSON([])
-		let samples = [199, 201, 300, 400, 500]
+		let samples = [150, 199, 300, 400, 500]
 
 		try samples.forEach { code in
 			XCTAssertThrowsError(
@@ -17,51 +17,65 @@ class ImageCommentsMapperTests: XCTestCase {
 		}
 	}
 
-	func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
+	func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
 		let invalidJSON = Data("invalid json".utf8)
+		let samples = [200, 205, 250, 270, 299]
 
-		XCTAssertThrowsError(
-			try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200))
-		)
+		try samples.forEach { code in
+			XCTAssertThrowsError(
+				try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+			)
+		}
 	}
 
-	func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
+	func test_map_deliversNoItemsOn2xxHTTPResponseWithEmptyJSONList() throws {
 		let emptyListJSON = makeItemsJSON([])
+		let samples = [200, 205, 250, 270, 299]
 
-		let result = try ImageCommentsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: 200))
+		try samples.forEach { code in
+			let result = try ImageCommentsMapper.map(emptyListJSON, from: HTTPURLResponse(statusCode: code))
 
-		XCTAssertEqual(result, [])
+			XCTAssertEqual(result, [])
+		}
 	}
 
-	func test_map_deliversItemsOn200HTTPResponseWithJSONItems() throws {
+	func test_map_deliversItemsOn2xxHTTPResponseWithJSONItems() throws {
 		let item1 = makeItem(
 			id: UUID(),
-			imageURL: URL(string: "http://a-url.com")!)
+			message: "a message",
+			createdAt: ("2021-05-31T02:53:14+00:00", Date(timeIntervalSince1970: 1622429594)),
+			username: "a username")
 
 		let item2 = makeItem(
 			id: UUID(),
-			description: "a description",
-			location: "a location",
-			imageURL: URL(string: "http://another-url.com")!)
+			message: "another message",
+			createdAt: ("2020-05-31T02:41:40-0700", Date(timeIntervalSince1970: 1590918100)),
+			username: "another username")
 
 		let json = makeItemsJSON([item1.json, item2.json])
 
-		let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: 200))
+		let samples = [200, 205, 250, 270, 299]
 
-		XCTAssertEqual(result, [item1.model, item2.model])
+		try samples.forEach { code in
+			let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+
+			XCTAssertEqual(result, [item1.model, item2.model])
+		}
 	}
 
 	// MARK: - Helpers
 
-	private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedImage, json: [String: Any]) {
-		let item = FeedImage(id: id, description: description, location: location, url: imageURL)
+	private func makeItem(id: UUID, message: String, createdAt: (iso8601String: String, date: Date), username: String) -> (model: ImageComment, json: [String: Any]) {
+		let item = ImageComment(id: id, message: message, createdAt: createdAt.date, username: username)
 
-		let json = [
+		let json: [String: Any] = [
 			"id": id.uuidString,
-			"description": description,
-			"location": location,
-			"image": imageURL.absoluteString
-		].compactMapValues { $0 }
+			"message": message,
+			"created_at": createdAt.iso8601String,
+			"author": [
+				"username": username
+			]
+		]
 
 		return (item, json)
 	}
