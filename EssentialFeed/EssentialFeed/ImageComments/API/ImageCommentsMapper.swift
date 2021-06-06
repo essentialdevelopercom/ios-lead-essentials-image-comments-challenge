4b20b7ff61fd25b45e635998c81b2bc25a1df643
Mikhail Macnev <misha@macnev.ru>
2021-06-06 12:53:25 +0500

    added validation HTTPURLResponse to any 2xx status code

--- EssentialFeed/EssentialFeed/ImageComments/API/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/ImageComments/API/ImageCommentsMapper.swift
@@ -22,10 +22,14 @@ public final class ImageCommentsMapper {
 	}
 
 	public static func map(_ data: Data, from response: HTTPURLResponse) throws -> [FeedImage] {
-		guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+		guard ImageCommentsMapper.isOK(response), let root = try? JSONDecoder().decode(Root.self, from: data) else {
 			throw Error.invalidData
 		}
 
 		return root.images
+	}
+
+	private static func isOK(_ response: HTTPURLResponse) -> Bool {
+		200 ... 299 ~= response.statusCode
 	}
 }
