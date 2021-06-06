7ca12beec59f6ec7869166afaa700e8c0fb7c6e3
Mikhail Macnev <misha@macnev.ru>
2021-06-06 13:55:16 +0500

    replace FeedPresenter with ImageCommentsPresenter

--- EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentsPresenterTests.swift
@@ -4,13 +4,13 @@ import EssentialFeed
 
 class ImageCommentsPresenterTests: XCTestCase {
 	func test_title_isLocalized() {
-		XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+		XCTAssertEqual(ImageCommentsPresenter.title, localized("FEED_VIEW_TITLE"))
 	}
 
 	func test_map_createsViewModel() {
 		let feed = uniqueImageFeed().models
 
-		let viewModel = FeedPresenter.map(feed)
+		let viewModel = ImageCommentsPresenter.map(feed)
 
 		XCTAssertEqual(viewModel.feed, feed)
 	}
@@ -19,7 +19,7 @@ class ImageCommentsPresenterTests: XCTestCase {
 
 	private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
 		let table = "Feed"
-		let bundle = Bundle(for: FeedPresenter.self)
+		let bundle = Bundle(for: ImageCommentsPresenter.self)
 		let value = bundle.localizedString(forKey: key, value: nil, table: table)
 		if value == key {
 			XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
