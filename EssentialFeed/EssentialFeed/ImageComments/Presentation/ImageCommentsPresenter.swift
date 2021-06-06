56cc2d0bdc7e0919699a5bb19c821bfd2aa2c061
Mikhail Macnev <misha@macnev.ru>
2021-06-06 13:51:56 +0500

    duplicate FeedPresenter to ImageCommentsPresenter

---
+++ EssentialFeed/EssentialFeed/ImageComments/Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+public final class ImageCommentsPresenter {
+	public static var title: String {
+		NSLocalizedString(
+			"FEED_VIEW_TITLE",
+			tableName: "Feed",
+			bundle: Bundle(for: FeedPresenter.self),
+			comment: "Title for the feed view")
+	}
+
+	public static func map(_ feed: [FeedImage]) -> FeedViewModel {
+		FeedViewModel(feed: feed)
+	}
+}
