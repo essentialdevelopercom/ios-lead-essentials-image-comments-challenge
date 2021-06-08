849819ab6efaf220b1a5ddea9975bb9e2419f76e
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-06-07 20:54:12 -0500

    Move ImageCommentViewModel to its own file

--- EssentialFeed/EssentialFeed/Image Comments/Presentation/ImageCommentsPresenter.swift
+++ EssentialFeed/EssentialFeed/Image Comments/Presentation/ImageCommentsPresenter.swift
@@ -4,18 +4,6 @@
 
 import Foundation
 
-public struct ImageCommentViewModel: Hashable {
-	public let message: String
-	public let date: String
-	public let username: String
-
-	public init(message: String, date: String, username: String) {
-		self.message = message
-		self.date = date
-		self.username = username
-	}
-}
-
 public final class ImageCommentsPresenter {
 	public static var title: String {
 		NSLocalizedString(
