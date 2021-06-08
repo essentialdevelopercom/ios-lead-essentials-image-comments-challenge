ea7de0191eda4301655c853b5e304d75bb9b03ab
Misha Macnev <misha@macnev.ru>
2021-06-08 23:14:44 +0500

    remove `ImageCommentStub`

--- EssentialFeed/EssentialFeediOSTests/ImageComments UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageComments UI/ImageCommentsSnapshotTests.swift
@@ -7,7 +7,7 @@ class ImageCommentsSnapshotTests: XCTestCase {
 	func test_imageCommentsWithContent() {
 		let sut = makeSUT()
 
-		sut.display(imageCommentsWithContent())
+		sut.display(comments())
 
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "IMAGE_COMMENT_WITH_CONTENT_light")
 		assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "IMAGE_COMMENT_WITH_CONTENT_dark")
@@ -26,44 +26,30 @@ class ImageCommentsSnapshotTests: XCTestCase {
 		return controller
 	}
 
-	private func imageCommentsWithContent() -> [ImageCommentStub] {
+	private func imageCommentsWithContent() -> [ImageCommentCellController] {
 		return [
-			ImageCommentStub(
-				message: "short message",
-				date: "1 day ago",
-				username: "A."
-			),
-			ImageCommentStub(
-				message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
-				date: "1000 years ago",
-				username: "long long long username"
-			),
-			ImageCommentStub(
-				message: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
-				date: "13 min ago",
-				username: "Username"
-			)
+			ImageCommentCellController(
+				viewModel: ImageCommentViewModel(
+					message: "short message",
+					date: "1 day ago",
+					username: "A."
+				)),
+			ImageCommentCellController(
+				viewModel: ImageCommentViewModel(
+					message: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+					date: "1000 years ago",
+					username: "long long long username"
+				)),
+			ImageCommentCellController(
+				viewModel: ImageCommentViewModel(
+					message: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+					date: "13 min ago",
+					username: "Username"
+				))
 		]
 	}
-}
 
-private extension ListViewController {
-	func display(_ stubs: [ImageCommentStub]) {
-		let cells: [CellController] = stubs.map { stub in
-			let cellController = ImageCommentCellController(viewModel: stub.viewModel)
-			stub.controller = cellController
-			return CellController(id: UUID(), cellController)
-		}
-
-		display(cells)
-	}
-}
-
-private class ImageCommentStub {
-	let viewModel: ImageCommentViewModel
-	weak var controller: ImageCommentCellController?
-
-	init(message: String, date: String, username: String) {
-		self.viewModel = ImageCommentViewModel(message: message, date: date, username: username)
+	private func comments() -> [CellController] {
+		imageCommentsWithContent().map { CellController(id: UUID(), $0) }
 	}
 }
