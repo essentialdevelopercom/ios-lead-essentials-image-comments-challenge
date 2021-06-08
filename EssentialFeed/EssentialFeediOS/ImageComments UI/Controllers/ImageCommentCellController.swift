bfba244980bcc97d8f08b855fc18252653dce3ba
Misha Macnev <misha@macnev.ru>
2021-06-08 22:48:06 +0500

    remove the `cell` property

--- EssentialFeed/EssentialFeediOS/ImageComments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeed/EssentialFeediOS/ImageComments UI/Controllers/ImageCommentCellController.swift
@@ -4,7 +4,6 @@ import EssentialFeed
 
 public final class ImageCommentCellController: NSObject {
 	private let viewModel: ImageCommentViewModel
-	private var cell: ImageCommentCell?
 
 	public init(viewModel: ImageCommentViewModel) {
 		self.viewModel = viewModel
@@ -17,18 +16,10 @@ extension ImageCommentCellController: UITableViewDataSource, UITableViewDelegate
 	}
 
 	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		cell = tableView.dequeueReusableCell()
-		cell?.labelUsername.text = viewModel.username
-		cell?.labelDate.text = viewModel.date
-		cell?.labelMessage.text = viewModel.message
-		return cell!
-	}
-
-	public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-		releaseCellForReuse()
-	}
-
-	private func releaseCellForReuse() {
-		cell = nil
+		let cell: ImageCommentCell = tableView.dequeueReusableCell()
+		cell.labelUsername.text = viewModel.username
+		cell.labelDate.text = viewModel.date
+		cell.labelMessage.text = viewModel.message
+		return cell
 	}
 }
