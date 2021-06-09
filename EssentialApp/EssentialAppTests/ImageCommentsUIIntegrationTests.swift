b09d166078b539defbab7a8ab1f1850af4e4bb39
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-06-08 21:02:03 -0500

    Load actions requests comments

--- EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
@@ -3,6 +3,7 @@
 //
 
 import XCTest
+import Combine
 import UIKit
 import EssentialApp
 import EssentialFeed
@@ -17,18 +18,18 @@ class ImageCommentsUIIntegrationTests: FeedUIIntegrationTests {
 		XCTAssertEqual(sut.title, commentsTitle)
 	}
 
-	override func test_loadFeedActions_requestFeedFromLoader() {
+	func test_loadCommentActions_requestCommentsFromLoader() {
 		let (sut, loader) = makeSUT()
-		XCTAssertEqual(loader.loadFeedCallCount, 0, "Expected no loading requests before view is loaded")
+		XCTAssertEqual(loader.loadCommentsCallCount, 0, "Expected no loading requests before view is loaded")
 
 		sut.loadViewIfNeeded()
-		XCTAssertEqual(loader.loadFeedCallCount, 1, "Expected a loading request once view is loaded")
+		XCTAssertEqual(loader.loadCommentsCallCount, 1, "Expected a loading request once view is loaded")
 
 		sut.simulateUserInitiatedReload()
-		XCTAssertEqual(loader.loadFeedCallCount, 2, "Expected another loading request once user initiates a reload")
+		XCTAssertEqual(loader.loadCommentsCallCount, 2, "Expected another loading request once user initiates a reload")
 
 		sut.simulateUserInitiatedReload()
-		XCTAssertEqual(loader.loadFeedCallCount, 3, "Expected yet another loading request once user initiates another reload")
+		XCTAssertEqual(loader.loadCommentsCallCount, 3, "Expected yet another loading request once user initiates another reload")
 	}
 
 	override func test_loadingFeedIndicator_isVisibleWhileLoadingFeed() {
@@ -149,5 +150,28 @@ class ImageCommentsUIIntegrationTests: FeedUIIntegrationTests {
 
 	private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "http://any-url.com")!) -> FeedImage {
 		return FeedImage(id: UUID(), description: description, location: location, url: url)
+	}
+
+	private class LoaderSpy {
+		private var requests = [PassthroughSubject<[FeedImage], Error>]()
+
+		var loadCommentsCallCount: Int {
+			return requests.count
+		}
+
+		func loadPublisher() -> AnyPublisher<[FeedImage], Error> {
+			let publisher = PassthroughSubject<[FeedImage], Error>()
+			requests.append(publisher)
+			return publisher.eraseToAnyPublisher()
+		}
+
+		func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
+			requests[index].send(feed)
+		}
+
+		func completeFeedLoadingWithError(at index: Int = 0) {
+			let error = NSError(domain: "an error", code: 0)
+			requests[index].send(completion: .failure(error))
+		}
 	}
 }
