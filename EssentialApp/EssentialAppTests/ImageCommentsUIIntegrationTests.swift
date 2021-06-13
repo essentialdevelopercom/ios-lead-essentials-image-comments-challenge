2c5e64810b6a13d4dd620423f9a9fa6f27967928
Vita Solomina <vitaliaa@gmail.com>
2021-06-13 13:55:43 -0700

    load comments with error renders error

--- EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
@@ -102,7 +102,7 @@ class ImageCommentsUIIntegrationTests: FeedUIIntegrationTests {
 		wait(for: [exp], timeout: 1.0)
 	}
 
-	override func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
+	func test_loadCommentsCompletion_rendersErrorMessageOnErrorUntilNextReload() {
 		let (sut, loader) = makeSUT()
 
 		sut.loadViewIfNeeded()
