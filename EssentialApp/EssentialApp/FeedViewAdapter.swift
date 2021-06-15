e6239d0a09a3e0fd5ab442791134869794416546
Vita Solomina <vitaliaa@gmail.com>
2021-06-14 17:12:58 -0700

    Move CommentsViewAdapter to its own file

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -53,17 +53,3 @@ extension UIImage {
 		return image
 	}
 }
-
-final class CommentsViewAdapter: ResourceView {
-	private weak var controller: ListViewController?
-
-	init(controller: ListViewController) {
-		self.controller = controller
-	}
-
-	func display(_ viewModel: ImageCommentsViewModel) {
-		controller?.display(viewModel.comments.map { viewModel in
-			CellController(id: viewModel, ImageCommentCellController(viewModel: viewModel))
-		})
-	}
-}
