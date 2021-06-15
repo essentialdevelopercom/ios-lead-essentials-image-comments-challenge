a6c9b1f709534ee3b7f499d3a83263b008a096fe
Vita Solomina <vitaliaa@gmail.com>
2021-06-14 17:15:38 -0700

    Use hardcoded UUID string to avoid extra logic in test

--- EssentialApp/EssentialApp/CommentsViewAdapter.swift
+++ EssentialApp/EssentialApp/CommentsViewAdapter.swift
@@ -1,4 +1,4 @@
-//	
+//
 // Copyright © 2021 Essential Developer. All rights reserved.
 //
 
@@ -7,15 +7,15 @@ import EssentialFeed
 import EssentialFeediOS
 
 final class CommentsViewAdapter: ResourceView {
-    private weak var controller: ListViewController?
+	private weak var controller: ListViewController?
 
-    init(controller: ListViewController) {
-        self.controller = controller
-    }
+	init(controller: ListViewController) {
+		self.controller = controller
+	}
 
-    func display(_ viewModel: ImageCommentsViewModel) {
-        controller?.display(viewModel.comments.map { viewModel in
-            CellController(id: viewModel, ImageCommentCellController(viewModel: viewModel))
-        })
-    }
+	func display(_ viewModel: ImageCommentsViewModel) {
+		controller?.display(viewModel.comments.map { viewModel in
+			CellController(id: viewModel, ImageCommentCellController(viewModel: viewModel))
+		})
+	}
 }
