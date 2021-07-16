7516d9ea4fd24e60cae64ac60bcfc7037ce4aae0
MING-TA YANG <startover205@gmail.com>
2021-07-16 09:53:21 +0800

    ImageCommentsMapper.map throws error on 2xx HTTP Response with invalid JSON

--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsMapperTests.swift
@@ -17,12 +17,16 @@ class ImageCommentsMapperTests: XCTestCase {
 		}
 	}
 
-	func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
+	func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
 		let invalidJSON = Data("invalid json".utf8)
 
-		XCTAssertThrowsError(
-			try FeedItemsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200))
-		)
+		let samples = [200, 201, 202, 203, 299]
+
+		try samples.forEach { code in
+			XCTAssertThrowsError(
+				try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+			)
+		}
 	}
 
 	func test_map_deliversNoItemsOn200HTTPResponseWithEmptyJSONList() throws {
