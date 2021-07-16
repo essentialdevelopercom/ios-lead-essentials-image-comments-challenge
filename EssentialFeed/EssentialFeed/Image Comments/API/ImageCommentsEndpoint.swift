5a3f4559fc810303932ed193a7a9fbb83f3e8016
MING-TA YANG <startover205@gmail.com>
2021-07-16 11:36:59 +0800

    ImageCommentsEndpoint.url returns correct URL

--- EssentialFeed/EssentialFeed/Image Comments/API/ImageCommentsEndpoint.swift
+++ EssentialFeed/EssentialFeed/Image Comments/API/ImageCommentsEndpoint.swift
@@ -1,5 +1,16 @@
-//	
+//
 // Copyright © 2021 Essential Developer. All rights reserved.
 //
 
 import Foundation
+
+public enum ImageCommentsEndpoint {
+	case get(imageID: String)
+
+	public func url(baseURL: URL) -> URL {
+		switch self {
+		case .get(let imageID):
+			return URL(string: baseURL.absoluteString.appending("/v1/image/\(imageID)/comments"))!
+		}
+	}
+}
--- EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/API/ImageCommentsEndpointTests.swift
@@ -1,10 +1,18 @@
-//	
+//
 // Copyright © 2021 Essential Developer. All rights reserved.
 //
 
 import XCTest
+import EssentialFeed
 
 class ImageCommentsEndpointTests: XCTestCase {
-    
+	func test_imageComments_endpintURL() {
+		let baseURL = URL(string: "http://base-url.com")!
+		let imageID = "an-image-id"
+		let expected = URL(string: "http://base-url.com/v1/image/an-image-id/comments")!
 
+		let received = ImageCommentsEndpoint.get(imageID: imageID).url(baseURL: baseURL)
+
+		XCTAssertEqual(received, expected)
+	}
 }
